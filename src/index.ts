2d5cee02d7830324623b230e0408d7dbf6663056
Jannis Leifeld <j.leifeld@shopware.com>
2022-06-14 11:26:15 +0200

    NEXT-17228 - remove dimensions from svg files

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,10 @@ import FigmaUtil from './figma/util';
 import * as fse from 'fs-extra';
 import chalk from 'chalk';
 import * as cliProgress from 'cli-progress';
+import type { OptimizedSvg } from 'svgo';
+import { optimize } from 'svgo';
+// @ts-expect-error - this dependency has no type definitions
+import * as svgoAutocrop from 'svgo-autocrop';
 
 const client = new FigmaApiClient();
 const util = new FigmaUtil();
@@ -18,24 +22,46 @@ client.getFile().then(async (response) => {
   console.log(chalk.green('Gathering icons...'));
   const iconMap = await util.buildIconMap(iconOverview);
 
-  console.log(chalk.green('Downloading icons...'));
+  console.log(chalk.green('Downloading and optimizing icons...'));
   const bar1 = new cliProgress.SingleBar({}, cliProgress.Presets.shades_classic);
 
   bar1.start(iconMap.size, 0);
 
   let count = 0;
   const promises: Promise<void>[] = [];
+
   iconMap.forEach((iconUrl, iconName) => {
-    const promise = client.downloadImage(iconUrl).then((result) => {
-      const svg = result.data as string;
+    const promise = Promise.resolve().then(async () => {
+      // Sleep for a random number below 10s to avoid too many connections at the same time
+      await new Promise(resolve => setTimeout(resolve, Math.floor(Math.random() * 10000)));
 
-      fse.outputFileSync(`${__dirname}/../${iconName}.svg`, svg);
-
-      ++count;
-
-      bar1.update(count);
-
-      return Promise.resolve();
+      return client.downloadImage(iconUrl).then((result) => {
+        const svg = result.data as string;
+  
+        // Remove width/height from SVGs
+        const optimizedSvgResult = optimize(svg, {
+          // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+          plugins: [
+            { name: 'removeDimensions' },
+            {
+              ...svgoAutocrop,
+              params: {
+                disableTranslateWarning: true,
+              },
+            },
+          ],
+        }) as OptimizedSvg;
+  
+        const optimizedSvg = optimizedSvgResult.data;
+  
+        fse.outputFileSync(`${__dirname}/../${iconName}.svg`, optimizedSvg);
+  
+        ++count;
+  
+        bar1.update(count);
+  
+        return Promise.resolve();
+      });
     });
 
     promises.push(promise);
