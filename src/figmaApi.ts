e2143d5f2109bf5afc623d880ea238b7853ad3df
Nils Haberkamp <n.haberkamp@shopware.com>
2024-01-19 16:05:29 +0100

    NEXT-32986 - add types to figmaApi

--- src/figmaApi.ts
+++ src/figmaApi.ts
@@ -1,4 +1,54 @@
 import { get } from "./httpClient";
+import { z } from "zod";
+
+const variableAlias = z.object({
+  id: z.string(),
+  type: z.literal("VARIABLE_ALIAS"),
+});
+
+const colorValue = z.object({
+  r: z.number(),
+  g: z.number(),
+  b: z.number(),
+  a: z.number(),
+});
+
+const variableSchema = z.object({
+  id: z.string(),
+  name: z.string(),
+  key: z.string(),
+  variableCollectionId: z.string(),
+  resolvedType: z.enum(["BOOLEAN", "FLOAT", "STRING", "COLOR"]),
+  valuesByMode: z.record(
+    z.string(),
+    z.union([z.boolean(), z.number(), z.string(), variableAlias, colorValue])
+  ),
+  remote: z.boolean(),
+  description: z.string(),
+  hiddenFromPublishing: z.boolean(),
+  scopes: z.array(z.unknown()),
+  // codeSyntax: variableCodeSyntax,
+});
+
+const variableCollection = z.object({
+  id: z.string(),
+  name: z.string(),
+  key: z.string(),
+  modes: z.array(z.object({ modeId: z.string(), name: z.string() })),
+  defaultModeId: z.string(),
+  remote: z.boolean(),
+  hiddenFromPublishing: z.boolean(),
+  variableIds: z.array(z.string()),
+});
+
+const responseSchema = z.object({
+  status: z.number(),
+  error: z.boolean(),
+  meta: z.object({
+    variables: z.record(z.string(), variableSchema),
+    variableCollections: z.record(z.string(), variableCollection),
+  }),
+});
 
 type Config = {
   apiKey: string;
@@ -11,6 +61,6 @@ export class FigmaApi {
     return get(`https://api.figma.com/v1/files/${fileKey}/variables/local`, {
       Accept: "*/*",
       "X-Figma-Token": this.config.apiKey,
-    });
+    }).then((response) => responseSchema.parse(response));
   }
 }
