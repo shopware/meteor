7f166cfcf7e8fa6c4c001ac5951599ea73afbab7
Nils Haberkamp <n.haberkamp@shopware.com>
2024-01-19 11:07:46 +0100

    NEXT-32986 - add basic script

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+import { env } from "./env";
+import { FigmaApi } from "./figmaApi";
+
+const figmaApi = new FigmaApi({
+  apiKey: env.API_KEY,
+});
+
+const fileKey = "Krub3xbG0vMUdoL8vzEZ7l";
+figmaApi.getDesignTokens(fileKey);
