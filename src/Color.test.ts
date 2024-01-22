24246f7cb9dc50a8a71f24fca018a72fdcb51adb
Nils Haberkamp <n.haberkamp@shopware.com>
2024-01-22 11:05:19 +0100

    NEXT-32986 - add color value object

---
+++ src/Color.test.ts
@@ -0,0 +1,23 @@
+import { expect, test as fact } from "vitest";
+import { Color } from "./Color";
+
+fact("it creates a hex color out of an RGB value", () => {
+  // GIVEN
+  const subject = Color;
+
+  // WHEN
+  const result = subject.fromRGB(0, 0, 255);
+
+  // THEN
+  expect(result.toHex()).toBe("#0000ff");
+});
+
+fact("it creates a hex color out of an RGBA value", () => {
+  const subject = Color;
+
+  // WHEN
+  const result = subject.fromRGB(0, 0, 255, 0);
+
+  // THEN
+  expect(result.toHex()).toBe("#0000ff00");
+});
---
+++ src/Color.ts
@@ -0,0 +1,40 @@
+import { z } from "zod";
+
+const schema = z.object({
+  red: z.number().min(0).max(255),
+  green: z.number().min(0).max(255),
+  blue: z.number().min(0).max(255),
+  alpha: z.number().min(0).max(1).default(1),
+});
+
+export class Color {
+  constructor(private value: z.infer<typeof schema>) {
+    schema.parse(value);
+  }
+
+  public static fromRGB(red: number, green: number, blue: number, alpha = 1) {
+    return new Color({
+      red,
+      green,
+      blue,
+      alpha,
+    });
+  }
+
+  public toHex() {
+    const toHex = (value: number) => {
+      const hex = Math.round(value).toString(16);
+
+      return hex.length === 1 ? "0" + hex : hex;
+    };
+
+    const hex = [
+      toHex(this.value.red),
+      toHex(this.value.green),
+      toHex(this.value.blue),
+    ].join("");
+
+    const containsAlpha = this.value.alpha !== 1;
+    return `#${hex}` + (containsAlpha ? toHex(this.value.alpha) : "");
+  }
+}
