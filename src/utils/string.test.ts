10eea4bdcfc4fe1bfc958fa9d6488cbfeddd0756
Nils Haberkamp <n.haberkamp@shopware.com>
2024-01-24 16:43:31 +0100

    NEXT-32986 - add utility method for turning string into kebab case

---
+++ src/utils/string.test.ts
@@ -0,0 +1,19 @@
+import { expect, test as fact } from "vitest";
+import { kebabCase } from "./string";
+
+fact.each([
+  "Slate / 50",
+  " Slate / 50 ",
+  "Slate     /     50 ",
+  "slate.50",
+  "slate / 50",
+])("turns %s into 'slate.50'", (input) => {
+  // GIVEN
+  const subject = kebabCase;
+
+  // WHEN
+  const result = subject(input);
+
+  // THEN
+  expect(result).toBe("slate.50");
+});
