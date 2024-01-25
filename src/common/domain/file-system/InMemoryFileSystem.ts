6b0e2c47234f2264e0a50af14b761c65c90a6a87
Nils Haberkamp <n.haberkamp@shopware.com>
2024-01-25 11:50:08 +0100

    NEXT-32986 - added InMemoryFileSystem class

---
+++ src/common/domain/file-system/InMemoryFileSystem.ts
@@ -0,0 +1,20 @@
+import type { FileSystem } from "./FileSystem";
+
+export class InMemoryFileSystem implements FileSystem {
+  private readonly files: Map<string, string> = new Map();
+
+  saveFile(path: string, content: string): void {
+    this.files.set(path, content);
+  }
+
+  readFile(path: string): string {
+    const file = this.files.get(path);
+
+    const fileDoesNotExist = !file;
+    if (fileDoesNotExist) {
+      throw new Error(`File ${path} does not exist.`);
+    }
+
+    return file;
+  }
+}
