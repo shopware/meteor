d5284a860e655ebb7f82519b712b0ad3e522cd27
Nils Haberkamp <n.haberkamp@shopware.com>
2024-01-25 10:20:13 +0100

    NEXT-32986 - allow HardDiskFileSystem to write file in nested directory

--- src/common/infrastructure/file-system/HardDiskFileSystem.test.ts
+++ src/common/infrastructure/file-system/HardDiskFileSystem.test.ts
@@ -3,11 +3,18 @@ import { HardDiskFileSystem } from "./HardDiskFileSystem";
 import fs from "node:fs";
 import path from "node:path";
 
+function cleanUp(pathToFile: string) {
+  fs.existsSync(pathToFile) && fs.unlinkSync(pathToFile);
+
+  const directory = path.join(__dirname, "./nested");
+  fs.existsSync(directory) && fs.rmdirSync(directory);
+}
+
 fact("creates a file that did not exist before", () => {
   // GIVEN
   const pathToFile = path.join(__dirname, "./test.txt");
+  cleanUp(pathToFile);
 
-  fs.existsSync(pathToFile) && fs.unlinkSync(pathToFile);
   const subject = new HardDiskFileSystem();
 
   // WHEN
@@ -16,4 +23,32 @@ fact("creates a file that did not exist before", () => {
   // THEN
   const result = fs.readFileSync(pathToFile, "utf8");
   expect(result).toBe("Hello, World!");
+
+  // TEARDOWN
+  cleanUp(pathToFile);
 });
+
+fact(
+  "creates a new file in a nested directory that did not exist before",
+  () => {
+    // GIVEN
+    const pathToFile = path.join(__dirname, "./nested/test.txt");
+    cleanUp(pathToFile);
+
+    const subject = new HardDiskFileSystem();
+
+    // WHEN
+    subject.saveFile(pathToFile, "Hello, Hell!");
+
+    // THEN
+    const result = fs.readFileSync(pathToFile, "utf8");
+    expect(result).toBe("Hello, Hell!");
+
+    // TEARDOWN
+    cleanUp(pathToFile);
+  }
+);
+
+fact.todo("overwrites a file that already exists");
+
+fact.todo("overwrites a file in a nested directory that already exists");
--- src/common/infrastructure/file-system/HardDiskFileSystem.ts
+++ src/common/infrastructure/file-system/HardDiskFileSystem.ts
@@ -1,8 +1,16 @@
 import type { FileSystem } from "../../domain/file-system/FileSystem";
 import fs from "node:fs";
+import { dirname } from "node:path";
 
 export class HardDiskFileSystem implements FileSystem {
   saveFile(path: string, content: string): void {
+    const directory = dirname(path);
+    const needToCreateTargetDirectory = !fs.existsSync(directory);
+
+    if (needToCreateTargetDirectory) {
+      fs.mkdirSync(directory, { recursive: true });
+    }
+
     fs.writeFileSync(path, content, { encoding: "utf-8" });
   }
 }
