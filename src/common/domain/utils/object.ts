f10ea0fa8ea836bbafd8edd0da7f8a670842635d
Nils Haberkamp <n.haberkamp@shopware.com>
2024-01-29 09:53:55 +0100

    NEXT-32986 - fix a handful of ESLint issues

--- src/common/domain/utils/object.ts
+++ src/common/domain/utils/object.ts
@@ -1,37 +1,52 @@
-export function set(target: Record<string, any>, path: string, value: any) {
+export function set<TValue>(
+  target: Record<string, unknown>,
+  path: string,
+  value: TValue,
+) {
   const keys = path.split('.');
   let currentObj = target;
 
   for (let i = 0; i < keys.length - 1; i++) {
     const key = keys[i];
+    if (!key)
+      throw new Error('Failed to set property on object: Key does not exist.');
 
-    if (!currentObj.hasOwnProperty(key)) {
+    if (!(key in currentObj)) {
       currentObj[key] = {};
     } else if (typeof currentObj[key] !== 'object') {
       throw new Error(`Invalid path: "${key}" is not an object`);
     }
 
-    currentObj = currentObj[key];
+    currentObj = currentObj[key] as Record<string, unknown>;
   }
 
   const lastKey = keys[keys.length - 1];
+  if (!lastKey)
+    throw new Error(
+      'Failed to set property on object: Last key does not exist.',
+    );
+
   currentObj[lastKey] = value;
 
   return target;
 }
 
-export function get(target: Record<string, any>, path: string) {
+export function get(target: Record<string, unknown>, path: string) {
   const keys = path.split('.');
   let currentObj = target;
 
   for (let i = 0; i < keys.length; i++) {
     const key = keys[i];
+    if (!key)
+      throw new Error(
+        'Failed to access property of object: Key does not exist.',
+      );
 
-    if (!currentObj.hasOwnProperty(key)) {
+    if (!(key in currentObj)) {
       return undefined;
     }
 
-    currentObj = currentObj[key];
+    currentObj = currentObj[key] as Record<string, unknown>;
   }
 
   return currentObj;
