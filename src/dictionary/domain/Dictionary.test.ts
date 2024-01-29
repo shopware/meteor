fd640a5d4afadf1b9965c9ca0f96d3beae44b75d
Nils Haberkamp <n.haberkamp@shopware.com>
2024-01-29 09:38:32 +0100

    NEXT-32986 - remove unnecessary it's in test names

--- src/dictionary/domain/Dictionary.test.ts
+++ src/dictionary/domain/Dictionary.test.ts
@@ -60,73 +60,70 @@ fact('creates a dictionary out of Figma Variables', () => {
   });
 });
 
-fact(
-  'it creates a dictionary with nested Tokens out of Figma Variables',
-  () => {
-    // GIVEN
-    const response: FigmaApiResponse = {
-      status: 200,
-      error: false,
-      meta: {
-        variables: {
-          'VariableID:11953:115880': {
-            id: 'VariableID:11953:115880',
-            name: 'Neutrals / Gray / 50',
-            key: 'db9aa5d3b7c6f03b4cddb78e045b566fae112d17',
-            variableCollectionId: 'VariableCollectionId:11953:115879',
-            resolvedType: 'COLOR',
-            valuesByMode: {
-              '11953:0': {
-                r: 0.9764705896377563,
-                g: 0.9803921580314636,
-                b: 0.9843137264251709,
-                a: 1,
-              },
+fact('creates a dictionary with nested Tokens out of Figma Variables', () => {
+  // GIVEN
+  const response: FigmaApiResponse = {
+    status: 200,
+    error: false,
+    meta: {
+      variables: {
+        'VariableID:11953:115880': {
+          id: 'VariableID:11953:115880',
+          name: 'Neutrals / Gray / 50',
+          key: 'db9aa5d3b7c6f03b4cddb78e045b566fae112d17',
+          variableCollectionId: 'VariableCollectionId:11953:115879',
+          resolvedType: 'COLOR',
+          valuesByMode: {
+            '11953:0': {
+              r: 0.9764705896377563,
+              g: 0.9803921580314636,
+              b: 0.9843137264251709,
+              a: 1,
             },
-            remote: false,
-            description: '',
-            hiddenFromPublishing: false,
-            scopes: ['ALL_SCOPES'],
           },
-        },
-        variableCollections: {
-          'VariableCollectionId:11953:115879': {
-            id: 'VariableCollectionId:11953:115879',
-            name: '.Design Tokens',
-            key: '9130479ef323598b1ccfb32e7b16dc80fcb30f14',
-            modes: [{ modeId: '11953:0', name: 'Default' }],
-            defaultModeId: '11953:0',
-            remote: false,
-            hiddenFromPublishing: true,
-            variableIds: ['VariableID:11953:115880'],
-          },
+          remote: false,
+          description: '',
+          hiddenFromPublishing: false,
+          scopes: ['ALL_SCOPES'],
         },
       },
-    };
-
-    const subject = Dictionary;
-
-    // WHEN
-    const result = subject.fromFigmaApiResponse(response, {
-      mode: 'Default',
-    }).value;
-
-    // THEN
-    expect(result).toStrictEqual({
-      neutrals: {
-        gray: {
-          50: {
-            $type: 'color',
-            $value: '#f9fafb',
-          },
+      variableCollections: {
+        'VariableCollectionId:11953:115879': {
+          id: 'VariableCollectionId:11953:115879',
+          name: '.Design Tokens',
+          key: '9130479ef323598b1ccfb32e7b16dc80fcb30f14',
+          modes: [{ modeId: '11953:0', name: 'Default' }],
+          defaultModeId: '11953:0',
+          remote: false,
+          hiddenFromPublishing: true,
+          variableIds: ['VariableID:11953:115880'],
         },
       },
-    });
-  },
-);
+    },
+  };
+
+  const subject = Dictionary;
+
+  // WHEN
+  const result = subject.fromFigmaApiResponse(response, {
+    mode: 'Default',
+  }).value;
+
+  // THEN
+  expect(result).toStrictEqual({
+    neutrals: {
+      gray: {
+        50: {
+          $type: 'color',
+          $value: '#f9fafb',
+        },
+      },
+    },
+  });
+});
 
 fact(
-  'it creates a dictionary containing an aliased Token out of Figma Variables',
+  'creates a dictionary containing an aliased Token out of Figma Variables',
   () => {
     // GIVEN
     const response: FigmaApiResponse = {
@@ -216,7 +213,7 @@ fact(
 );
 
 fact(
-  'it creates a dictionary containing aliased Tokens that reference Design Tokens from other files',
+  'creates a dictionary containing aliased Tokens that reference Design Tokens from other files',
   () => {
     // GIVEN
     const response: FigmaApiResponse = {
