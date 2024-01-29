c76736292f41758473a5e99a6c255060ec4a03e1
Nils Haberkamp <n.haberkamp@shopware.com>
2024-01-29 13:33:13 +0100

    NEXT-32986 - remove mws logs

--- vitest.setup.ts
+++ vitest.setup.ts
@@ -12,7 +12,3 @@ afterEach(() => {
 afterAll(() => {
   server.close();
 });
-
-server.events.on('request:start', ({ request }) => {
-  console.log('MSW intercepted:', request.method, request.url);
-});
