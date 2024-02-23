1ecc093806685e139a2ef27cf33ce8e43e83d5d9
Nils Haberkamp <n.haberkamp@shopware.com>
2024-02-23 13:35:04 +0100

    sync component library

---
+++ packages/component-library/src/components/form/sw-switch/sw-switch.vue
@@ -0,0 +1,413 @@
+<template>
+  <div class="sw-field--switch__container">
+    <div class="sw-field--switch" :class="swSwitchFieldClasses">
+      <div class="sw-field--switch__content">
+        <div class="sw-field--switch__input">
+          <!-- eslint-disable-next-line vuejs-accessibility/form-control-has-label -->
+          <input
+            :id="identification"
+            type="checkbox"
+            :name="formFieldName || identification"
+            :checked="inputState"
+            :disabled="isDisabled"
+            @change.stop="onChange"
+          />
+          <div class="sw-field__switch-state">
+            <div class="sw-field__switch-state-knob" />
+          </div>
+        </div>
+
+        <sw-base-field
+          v-bind="$attrs"
+          :disabled="disabled"
+          :required="required"
+          :name="identification"
+          :has-focus="false"
+          :help-text="helpText"
+          :is-inheritance-field="isInheritanceField"
+          :is-inherited="isInherited"
+          @inheritance-restore="onInheritanceRestore($event)"
+          @inheritance-remove="$emit('inheritance-remove', $event)"
+        >
+          <template #label>
+            {{ label }}
+          </template>
+        </sw-base-field>
+      </div>
+    </div>
+    <sw-field-error :error="error" :class="errorClasses" />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import SwBaseField from "../_internal/sw-base-field/sw-base-field.vue";
+import SwFieldError from "../_internal/sw-field-error/sw-field-error.vue";
+import SwFormFieldMixin from "../../../mixins/form-field.mixin";
+import { createId } from "../../../utils/uuid";
+
+export default defineComponent({
+  name: "SwSwitch",
+
+  components: {
+    "sw-base-field": SwBaseField,
+    "sw-field-error": SwFieldError,
+  },
+
+  mixins: [SwFormFieldMixin],
+
+  props: {
+    label: {
+      type: String,
+      required: false,
+      default: "",
+    },
+    required: {
+      type: Boolean,
+      default: false,
+    },
+    disabled: {
+      type: Boolean,
+      default: false,
+    },
+
+    /**
+     * Determines the checked state of the checkbox.
+     */
+    checked: {
+      type: Boolean,
+      required: false,
+      default: null,
+    },
+
+    /**
+     * Determines if the field is surrounded by a border.
+     */
+    bordered: {
+      type: Boolean,
+      required: false,
+      default: false,
+    },
+
+    /**
+     * Inherited value from another SalesChannel.
+     */
+    inheritedValue: {
+      type: Boolean,
+      required: false,
+      default: null,
+    },
+
+    /**
+     * Help text with additional information for the field.
+     */
+    helpText: {
+      type: String,
+      required: false,
+      default: null,
+    },
+
+    /**
+     * Error object for this field.
+     */
+    error: {
+      type: Object,
+      required: false,
+      default: null,
+    },
+
+    removeTopMargin: {
+      type: Boolean,
+      required: false,
+      default: false,
+    },
+
+    /**
+     * Name of the form field.
+     */
+    name: {
+      type: String,
+      required: false,
+      default: "",
+    },
+  },
+
+  data() {
+    return {
+      currentValue: this.checked,
+      id: createId(),
+    };
+  },
+
+  computed: {
+    identification(): string {
+      return `sw-field--${this.id}`;
+    },
+
+    inputState(): boolean {
+      if (this.isInherited) {
+        return this.inheritedValue;
+      }
+
+      return this.currentValue || false;
+    },
+
+    isInheritanceField(): boolean {
+      if (this.$attrs.isInheritanceField) {
+        return true;
+      }
+      return this.inheritedValue !== null;
+    },
+
+    isInherited(): boolean {
+      if (this.$attrs.isInherited) {
+        return true;
+      }
+      return this.isInheritanceField && this.currentValue === null;
+    },
+
+    hasError(): boolean {
+      return this.error && this.error.code !== 0;
+    },
+
+    swSwitchFieldClasses(): Record<string, boolean>[] {
+      return [
+        {
+          "has--error": this.hasError,
+          "sw-field--switch-bordered": this.bordered,
+          "sw-field--switch-no-margin-top": this.removeTopMargin,
+          "sw-field--switch-no-margin-bottom": this.hasError,
+        },
+      ];
+    },
+
+    errorClasses(): {
+      "sw-field__error--move-up": boolean;
+    }[] {
+      return [
+        {
+          "sw-field__error--move-up": !this.bordered,
+        },
+      ];
+    },
+
+    isDisabled(): boolean {
+      return this.disabled || this.isInherited;
+    },
+  },
+
+  watch: {
+    checked() {
+      this.currentValue = this.checked;
+    },
+  },
+
+  methods: {
+    onChange(changeEvent: Event) {
+      // @ts-expect-error - target exists on event
+      this.$emit("change", changeEvent.target.checked);
+    },
+
+    onInheritanceRestore(event: Event) {
+      this.$emit("inheritance-restore", event);
+    },
+  },
+});
+</script>
+
+<style lang="scss">
+@import "../../assets/scss/variables.scss";
+
+$sw-field--switch-color-border: $color-gray-300;
+$sw-field--switch-color-background: $color-white;
+$sw-field--switch-color-text: $color-darkgray-200;
+$sw-field--switch-color-focus: $color-shopware-brand-500;
+$sw-field--switch-color-error: $color-crimson-500;
+
+.sw-field--switch__container {
+  .sw-field--switch {
+    margin-top: 24px;
+    margin-bottom: 22px;
+
+    &.sw-field--switch-no-margin-top {
+      margin-top: 0;
+    }
+
+    &.sw-field--switch-no-margin-bottom {
+      margin-bottom: 0;
+    }
+
+    .sw-field__error {
+      margin-top: -14px;
+    }
+
+    .sw-field {
+      margin-bottom: 0;
+
+      .sw-block-field__block {
+        border: none;
+      }
+    }
+
+    .sw-field__label {
+      margin-bottom: 0;
+
+      label {
+        cursor: pointer;
+        flex-grow: initial;
+        padding: 15px 0 15px 4px;
+      }
+    }
+
+    .sw-field--switch__content {
+      display: grid;
+      grid-template-columns: 24px 1fr auto;
+      align-items: center;
+      color: $sw-field--switch-color-text;
+    }
+
+    &.sw-field--switch-bordered {
+      .sw-field__error {
+        padding: 0 16px;
+      }
+    }
+
+    &.sw-field--switch-bordered .sw-field--switch__content {
+      border-radius: 4px;
+      border: 1px solid $sw-field--switch-color-border;
+      padding: 0 16px;
+    }
+
+    .sw-field--switch__input {
+      position: relative;
+      padding: 15px 0;
+      width: 24px;
+      height: 100%;
+
+      input[type="checkbox"] {
+        opacity: 0;
+        display: block;
+        width: 100%;
+        height: 100%;
+        position: absolute;
+        top: 0;
+        left: 0;
+        border: 0 none;
+        background: none;
+        -webkit-appearance: none;
+        z-index: 2;
+        cursor: pointer;
+
+        &:disabled {
+          cursor: auto;
+        }
+
+        &:disabled ~ .sw-field__switch-state {
+          background: $color-gray-100;
+
+          .sw-field__switch-state-knob {
+            background: $sw-field--switch-color-border;
+          }
+        }
+
+        &:checked ~ .sw-field__switch-state {
+          background: $sw-field--switch-color-focus;
+
+          .sw-field__switch-state-knob {
+            left: 10px;
+          }
+        }
+
+        &:checked:disabled ~ .sw-field__switch-state {
+          background: $color-gray-100;
+        }
+      }
+
+      .sw-field__switch-state {
+        position: absolute;
+        width: 100%;
+        height: 16px;
+        z-index: 1;
+        text-align: center;
+        background: $sw-field--switch-color-border;
+        border-radius: 8px;
+
+        .sw-field__switch-state-knob {
+          transition: all 0.3s ease-in-out;
+          width: 10px;
+          height: 10px;
+          position: absolute;
+          top: 3px;
+          left: 3px;
+          background: $sw-field--switch-color-background;
+          border-radius: 7px;
+        }
+      }
+    }
+
+    .sw-field__inheritance-icon {
+      display: flex;
+      align-items: center;
+      margin-left: 8px;
+      margin-right: 0;
+    }
+
+    &.has--error {
+      .sw-block-field__block {
+        background: $color-crimson-50;
+      }
+
+      &.sw-field--switch-bordered {
+        .sw-field--switch__content {
+          border: 1px solid $sw-field--switch-color-error;
+          background: $color-crimson-50;
+        }
+      }
+
+      .sw-field__switch-state {
+        background: $sw-field--switch-color-error;
+      }
+
+      input[type="checkbox"] {
+        &:disabled ~ .sw-field__switch-state {
+          background: $sw-field--switch-color-error;
+
+          .sw-field__switch-state-knob {
+            background: darken($sw-field--switch-color-error, 5%);
+          }
+        }
+
+        &:checked ~ .sw-field__switch-state {
+          background: $sw-field--switch-color-error;
+        }
+
+        &:checked:disabled ~ .sw-field__switch-state {
+          background: $sw-field--switch-color-error;
+        }
+      }
+    }
+
+    &.is--inherited {
+      input[type="checkbox"] {
+        &:checked ~ .sw-field__switch-state {
+          background: $sw-field--switch-color-border;
+        }
+      }
+    }
+
+    &:focus-within {
+      .sw-field__switch-state {
+        border-color: $color-shopware-brand-500;
+        box-shadow: 0 0 4px $color-shopware-brand-500;
+      }
+    }
+  }
+
+  .sw-field__error {
+    &--move-up {
+      transform: translateY(-19px);
+      margin-bottom: 21px;
+    }
+  }
+}
+</style>
---
+++ packages/component-library/src/components/form/sw-text-field/sw-text-field.vue
@@ -0,0 +1,270 @@
+<template>
+  <sw-base-field
+    :disabled="disabled"
+    :required="required"
+    :is-inherited="isInherited"
+    :is-inheritance-field="isInheritanceField"
+    :disable-inheritance-toggle="disableInheritanceToggle"
+    :copyable="copyable"
+    :copyable-tooltip="copyableTooltip"
+    :copyable-text="currentValue"
+    :has-focus="hasFocus"
+    :help-text="helpText"
+    :name="name"
+    :size="size"
+    @inheritance-restore="$emit('inheritance-restore', $event)"
+    @inheritance-remove="$emit('inheritance-remove', $event)"
+  >
+    <template #label>
+      {{ label }}
+    </template>
+
+    <template #field-prefix>
+      <slot name="prefix" />
+    </template>
+
+    <template #element="{ identification }">
+      <input
+        :id="createInputId(identification)"
+        type="text"
+        :name="identification"
+        :disabled="hasDisabledInput"
+        :value="currentValue"
+        :placeholder="placeholder"
+        @input="onInput"
+        @change="onChange"
+        @focus="setFocusClass"
+        @blur="removeFocusClass"
+      />
+    </template>
+
+    <template #field-suffix>
+      <slot name="suffix" />
+    </template>
+
+    <template #error>
+      <sw-field-error v-if="error" :error="error" />
+    </template>
+
+    <template #field-hint>
+      <slot name="hint" />
+    </template>
+  </sw-base-field>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import SwBaseField from "../_internal/sw-base-field/sw-base-field.vue";
+import SwFieldError from "../_internal/sw-field-error/sw-field-error.vue";
+
+export default defineComponent({
+  name: "SwTextField",
+
+  components: {
+    "sw-field-error": SwFieldError,
+    "sw-base-field": SwBaseField,
+  },
+
+  inheritAttrs: false,
+
+  props: {
+    /**
+     * The value of the text field.
+     */
+    modelValue: {
+      type: String,
+      required: false,
+      default: "",
+    },
+
+    /**
+     * A placeholder text being displayed if no value is set.
+     */
+    placeholder: {
+      type: String,
+      required: false,
+      default: "",
+    },
+
+    /**
+     * A label for your text field. Usually used to guide the user what value this field controls.
+     */
+    label: {
+      type: String,
+      required: false,
+      default: null,
+    },
+
+    /**
+     * A text that helps the user to understand what this field does.
+     */
+    helpText: {
+      type: String,
+      required: false,
+      default: null,
+    },
+
+    /**
+     * The size of the text field.
+     *
+     * @values small, default
+     */
+    size: {
+      type: String,
+      required: false,
+      default: "default",
+      validator(value: string) {
+        return ["small", "default"].includes(value);
+      },
+    },
+
+    /**
+     * Toggles the copy function of the text field.
+     */
+    copyable: {
+      type: Boolean,
+      required: false,
+      default: false,
+    },
+
+    /**
+     * If set to true the tooltip will change on successful copy.
+     */
+    copyableTooltip: {
+      type: Boolean,
+      required: false,
+      default: false,
+    },
+
+    /**
+     * An error in your business logic related to this field.
+     *
+     * @example {"code": 500, "detail": "Error while saving"}
+     */
+    error: {
+      type: Object,
+      required: false,
+      default: null,
+    },
+
+    /**
+     * Determines if the field is disabled.
+     */
+    disabled: {
+      type: Boolean,
+      required: false,
+      default: false,
+    },
+
+    /**
+     * Determines if the field is required.
+     */
+    required: {
+      type: Boolean,
+      required: false,
+      default: false,
+    },
+
+    /**
+     * Toggles the inheritance visualization.
+     */
+    isInherited: {
+      type: Boolean,
+      required: false,
+      default: false,
+    },
+
+    /**
+     * Determines if the field is inheritable.
+     */
+    isInheritanceField: {
+      type: Boolean,
+      required: false,
+      default: false,
+    },
+
+    /**
+     * Determines the active state of the inheritance toggle.
+     */
+    disableInheritanceToggle: {
+      type: Boolean,
+      required: false,
+      default: false,
+    },
+
+    /**
+     * @ignore
+     */
+    idSuffix: {
+      type: String,
+      required: false,
+      default: "",
+    },
+
+    /**
+     * @ignore
+     */
+    name: {
+      type: String,
+      required: false,
+      default: null,
+    },
+  },
+
+  data() {
+    return {
+      currentValue: this.modelValue,
+      hasFocus: false,
+    };
+  },
+
+  computed: {
+    hasError(): boolean {
+      // @ts-expect-error - isValid gets called in the mixin
+      return !this.isValid || !!this.error;
+    },
+
+    hasDisabledInput(): boolean {
+      return this.disabled || this.isInherited;
+    },
+  },
+
+  watch: {
+    modelValue(value) {
+      this.currentValue = value;
+    },
+  },
+
+  methods: {
+    onChange(event: Event): void {
+      // @ts-expect-error - target is defined
+      this.$emit("change", event.target.value || "");
+    },
+
+    onInput(event: Event): void {
+      // @ts-expect-error - target is defined
+      this.$emit("update:modelValue", event.target.value);
+    },
+
+    restoreInheritance(): void {
+      this.$emit("update:modelValue", null);
+    },
+
+    createInputId(identification: string): string {
+      if (!this.idSuffix || this.idSuffix.length <= 0) {
+        return identification;
+      }
+
+      return `${identification}-${this.idSuffix}`;
+    },
+
+    setFocusClass(): void {
+      this.hasFocus = true;
+    },
+
+    removeFocusClass(): void {
+      this.hasFocus = false;
+    },
+  },
+});
+</script>
