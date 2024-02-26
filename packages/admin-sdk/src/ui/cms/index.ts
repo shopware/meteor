85c804456cb9a0ae822de56856d78ee6e8d46355
Jannis Leifeld <j.leifeld@shopware.com>
2024-02-26 09:25:44 +0100

    NEXT-33669 - implement cms blocks for sdk

--- packages/admin-sdk/src/ui/cms/index.ts
+++ packages/admin-sdk/src/ui/cms/index.ts
@@ -1,6 +1,7 @@
 import { createSender } from '../../channel';
 
 export const registerCmsElement = createSender('cmsRegisterElement');
+export const registerCmsBlock = createSender('cmsRegisterBlock');
 
 export type cmsRegisterElement = {
     responseType: void,
@@ -28,3 +29,50 @@ export type cmsRegisterElement = {
         [key: string]: unknown,
     },
 };
+
+export type cmsRegisterBlock = {
+    responseType: void,
+
+    /**
+     * A unique technical name for your block. We recommend to use a shorthand prefix for your company, e.g. "Swag" for shopware AG.
+     */
+    name: string,
+
+    /**
+     * The category your block is associated with, e.g. "commerce", "form", "image", "sidebar", "text-image", "text", "video", etc.
+     * 
+     * New custom categories will get the following snippet key: "apps.sw-cms.detail.label.blockCategory.{categoryName}"
+     */
+    category?: ('commerce'|'form'|'image'|'sidebar'|'text-image'|'text'|'video') | string & Record<never, never>,
+
+    /**
+     * Your block's label which will be shown in the CMS module in the Administration.
+     */
+    label: string,
+
+    /**
+     * Define the slots of the block. Each slot is a single div. The element property is the name of the element that can be placed in the slot.
+     */
+    slots: Array<{
+        element: string,
+    }>,
+
+    /**
+     * Define the layout of the block. The grid property is a shorthand for the CSS grid property. The grid property is optional.
+     */
+    slotLayout?: {
+        /**
+         * The grid layout for the block. Each slot is a single div. Can be defined with the CSS shorthand property "grid":
+         * https://developer.mozilla.org/en-US/docs/Web/CSS/grid
+         * 
+         * Examples:
+         * 1 column layout example: "auto / auto"
+         * 2 column layout example: "auto / auto auto"
+         * 2 row layout example: "auto auto / auto-flow auto"
+         */
+        grid?: string,
+    },
+
+    // The preview image for the block. Minimum width should be 350px
+    previewImage?: string,
+}
