179e7026451787a85f7c98bd7c4d909a298ccbbb
Nils Haberkamp <haberkamp.n@gmail.com>
2024-03-15 16:41:55 +0100

    NEXT-34341 - hide base field hint when slot is not used

--- packages/component-library/src/components/form/_internal/mt-base-field/mt-base-field.vue
+++ packages/component-library/src/components/form/_internal/mt-base-field/mt-base-field.vue
@@ -38,7 +38,10 @@
 
     <slot name="error" />
 
-    <div class="mt-field__hint-wrapper">
+    <div
+      v-if="!!$slots['field-hint'] && !$slots['field-hint-right']"
+      class="mt-field__hint-wrapper"
+    >
       <div class="mt-field__hint">
         <slot name="field-hint" />
       </div>
