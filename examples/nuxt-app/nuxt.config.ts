336c599348ba0a9d3c82a762c91abf2bb6f7e4bc
Nils Haberkamp <haberkamp.n@gmail.com>
2024-03-21 16:22:21 +0100

    add snapshot testing for nuxt example app

--- examples/nuxt-app/nuxt.config.ts
+++ examples/nuxt-app/nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true },
+  devtools: { enabled: false },
   devServer: {
     host: '127.0.0.1',
     port: 3000,
