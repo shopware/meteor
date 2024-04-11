8c7d00119e9614091e72efba2bff6b3e903b1356
Nils Haberkamp <haberkamp.n@gmail.com>
2024-04-11 10:00:18 +0200

    make createId function compatible with node.js

--- packages/component-library/src/utils/uuid.ts
+++ packages/component-library/src/utils/uuid.ts
@@ -1,18 +1,11 @@
-function uuidv4() {
-  // @ts-expect-error
-  return ([1e7] + -1e3 + -4e3 + -8e3 + -1e11).replace(/[018]/g, (c) =>
-    (c ^ (window.crypto.getRandomValues(new Uint8Array(1))[0] & (15 >> (c / 4)))).toString(16),
-  );
+function isNode() {
+  return typeof process !== "undefined" && !!process.versions && !!process.versions.node;
 }
 
-/**
- * Returns a uuid string in hex format.
- *
- * @returns {String}
- */
 export function createId() {
-  // eslint-disable-next-line max-len
-  return uuidv4().replace(/-/g, "");
+  const crypto = isNode() ? require("crypto") : window.crypto;
+
+  return crypto.randomUUID();
 }
 
 export default {
