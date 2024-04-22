9d443bbde660aaeafc56f2c44bb0fa7e3d4cc10a
Nils Haberkamp <haberkamp.n@gmail.com>
2024-04-22 10:13:44 +0200

    NEXT-34341 - add data table filter helper

---
+++ packages/admin-sdk/src/data/data-table-filter.helper.ts
@@ -0,0 +1,15 @@
+import Criteria from './Criteria';
+import { type Filter } from '@shopware-ag/meteor-component-library';
+
+export function addDataTableFilters(
+  criteria: Criteria,
+  filters: Filter[]
+): Criteria {
+  filters.forEach((filter) => {
+    filter.type.options.forEach((option) => {
+      criteria.addFilter(Criteria.equals(filter.id, option.id));
+    });
+  });
+
+  return criteria;
+}
