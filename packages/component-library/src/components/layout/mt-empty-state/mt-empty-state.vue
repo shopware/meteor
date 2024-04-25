cfff3b8852a90f28f6a675162256e2a3a8d16356
Sy Dinh <s.dinh@shopware.com>
2024-04-25 13:39:31 +0700

    PS-224 - Update and build globally mt-empty-state (#143)

--- packages/component-library/src/components/layout/mt-empty-state/mt-empty-state.vue
+++ packages/component-library/src/components/layout/mt-empty-state/mt-empty-state.vue
@@ -11,19 +11,38 @@
     <p class="mt-empty-state__description">
       {{ description }}
     </p>
+
+    <mt-external-link v-if="linkHref && linkText" class="mt-empty-state__link" :href="linkHref">
+      {{ linkText }}
+    </mt-external-link>
+
+    <mt-button
+      v-if="buttonText"
+      class="mt-empty-state__button"
+      variant="primary"
+      @click="$emit('button-click')"
+    >
+      <mt-icon name="solid-plus-circle-s" />
+      {{ buttonText }}
+      <mt-icon name="solid-long-arrow-right" size="16px" />
+    </mt-button>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent, type PropType } from "vue";
 import type { TranslateResult } from "vue-i18n";
+import MtExternalLink from "@/components/form/mt-external-link/mt-external-link.vue";
+import MtButton from "@/components/form/mt-button/mt-button.vue";
 import MtIcon from "../../icons-media/mt-icon/mt-icon.vue";
 
 export default defineComponent({
   name: "MtEmptyState",
 
   components: {
-    "mt-icon": MtIcon,
+    MtIcon,
+    MtExternalLink,
+    MtButton,
   },
 
   props: {
@@ -42,7 +61,27 @@ export default defineComponent({
       required: false,
       default: "solid-content",
     },
+
+    linkHref: {
+      type: String as PropType<string | TranslateResult>,
+      required: false,
+      default: undefined,
+    },
+
+    linkText: {
+      type: String as PropType<string | TranslateResult>,
+      required: false,
+      default: undefined,
+    },
+
+    buttonText: {
+      type: String as PropType<string | TranslateResult>,
+      required: false,
+      default: undefined,
+    },
   },
+
+  emits: ["button-click"],
 });
 </script>
 
@@ -54,26 +93,35 @@ export default defineComponent({
     display: inline-block;
     padding: 12px;
     border-radius: $border-radius-default;
-    background-color: $color-gray-100;
+    background-color: var(--color-interaction-secondary-dark);
 
     svg {
-      color: $color-gray-800;
+      color: var(--color-icon-primary-default);
     }
   }
 
-  h2.mt-empty-state__headline {
-    color: $color-darkgray-800;
+  &__headline {
+    color: var(--color-text-primary-default);
     font-weight: $font-weight-bold;
     font-size: $font-size-l;
     line-height: $line-height-lg;
-    margin-top: 24px;
+    margin: 16px 0 0;
   }
 
-  p.mt-empty-state__description {
-    color: $color-darkgray-200;
+  &__description {
+    color: var(--color-text-secondary-default);
     font-size: $font-size-xs;
     line-height: $line-height-sm;
     margin-top: 8px;
+  }
+
+  &__link {
+    display: block;
+    margin-top: 8px;
+  }
+
+  &__button {
+    margin-top: 16px;
   }
 }
 </style>
