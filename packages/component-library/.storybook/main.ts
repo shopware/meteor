a72526c2ad84f8a0c26a3f8c39a1dc056f4e8f7f
Nils Haberkamp <haberkamp.n@gmail.com>
2024-05-22 08:27:27 +0200

    enable a11y plugin

--- packages/component-library/.storybook/main.ts
+++ packages/component-library/.storybook/main.ts
@@ -8,6 +8,7 @@ const config: StorybookConfig = {
     getAbsolutePath("@storybook/addon-essentials"),
     getAbsolutePath("@storybook/addon-interactions"),
     getAbsolutePath("storybook-dark-mode"),
+    getAbsolutePath("@storybook/addon-a11y"),
   ],
   framework: {
     name: getAbsolutePath("@storybook/vue3-vite"),
