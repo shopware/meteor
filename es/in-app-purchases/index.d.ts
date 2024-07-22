33c198ebc9ca5bff786750c346f84287ea38234c
Michel Bade <m.bade@shopware.com>
2024-07-22 16:56:18 +0200

    NEXT-36303 - Update

--- es/in-app-purchases/index.d.ts
+++ es/in-app-purchases/index.d.ts
@@ -4,7 +4,7 @@
 export declare const purchase: (messageOptions: import("../channel").MessageDataType<"inAppPurchase"> & import("../channel").BaseMessageOptions) => Promise<unknown>;
 export type inAppPurchase<T extends 'oneTime' | 'subscription' = 'oneTime' | 'subscription'> = {
     responseType: unknown;
-    featureId: string;
+    identifier: string;
     rentType: T;
     model: T extends 'subscription' ? 'yearly' | 'monthly' : undefined | null;
 };
