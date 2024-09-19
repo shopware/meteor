94a98e8e8e1ce83184001d15de099ab4db13f2d2
Nils <haberkamp.n@gmail.com>
2024-09-19 14:29:47 +0200

    migrate empty state to plain css

--- packages/component-library/src/components/layout/mt-empty-state/mt-empty-state.vue
+++ packages/component-library/src/components/layout/mt-empty-state/mt-empty-state.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="mt-empty-state">
     <div class="mt-empty-state__icon">
-      <mt-icon :name="icon" />
+      <mt-icon :name="icon" color="var(--color-icon-primary-default)" />
     </div>
 
     <mt-text as="h2" size="l" weight="bold" class="mt-empty-state__headline">
@@ -87,37 +87,25 @@ export default defineComponent({
 });
 </script>
 
-<style lang="scss">
-.mt-empty-state {
-  display: flex;
-  flex-direction: column;
-  align-items: flex-start;
+<style scoped>
+.mt-empty-state__icon {
+  display: inline-block;
+  padding: 0.75rem;
+  border-radius: var(--border-radius-xs);
+  background-color: var(--color-interaction-secondary-dark);
+  margin-block-end: 1.5rem;
+}
 
-  &__icon {
-    display: inline-block;
-    padding: 12px;
-    border-radius: var(--border-radius-xs);
-    background-color: var(--color-interaction-secondary-dark);
+.mt-empty-state__headline {
+  margin-block-end: 0.5rem;
+}
 
-    svg {
-      color: var(--color-icon-primary-default);
-    }
-  }
+.mt-empty-state__link {
+  margin-block-start: 0.5rem;
+  display: block;
+}
 
-  &__headline {
-    margin: 16px 0 0;
-  }
-
-  &__description {
-    margin-top: 8px;
-  }
-
-  &__link {
-    margin-top: 8px;
-  }
-
-  &__button {
-    margin-top: 16px;
-  }
+.mt-empty-state__button {
+  margin-block-start: 1.5rem;
 }
 </style>
