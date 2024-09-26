6416457a4754338984501d05bc1e48acabbf4a33
Nils <haberkamp.n@gmail.com>
2024-09-26 09:31:49 +0200

    move icon composition api

--- packages/component-library/src/components/icons-media/mt-icon/mt-icon.vue
+++ packages/component-library/src/components/icons-media/mt-icon/mt-icon.vue
@@ -2,7 +2,12 @@
   <!-- eslint-disable vue/no-v-html -->
   <span
     class="mt-icon"
-    :class="classes"
+    :class="[
+      `icon--${props.name}`,
+      {
+        'mt-icon--custom-size': !!props.size,
+      },
+    ]"
     :style="styles"
     :aria-hidden="decorative"
     :data-testid="'mt-icon__' + name"
@@ -11,118 +16,64 @@
   />
 </template>
 
-<script lang="ts">
-import { defineComponent } from "vue";
+<script setup lang="ts">
+import { computed, ref, watch, type CSSProperties } from "vue";
 
-export default defineComponent({
-  name: "MtIcon",
-
-  props: {
-    /**
-     * The type of the icon. You can use every icon from the meteor-icon-kit:
-     * https://shopware.github.io/meteor-icon-kit/
-     */
-    name: {
-      type: String,
-      required: true,
-    },
-    /**
-     * The color of the icon.
-     */
-    color: {
-      type: String,
-      required: false,
-      default: null,
-    },
-    /**
-     * If this is set to true then the icon is not detectable by screen reader. This should only
-     * be done if this icon is only decorative and has no further purpose.
-     */
-    decorative: {
-      type: Boolean,
-      required: false,
-      default: false,
-    },
-    size: {
-      type: String,
-      required: false,
-      default: undefined,
-    },
+const props = withDefaults(
+  defineProps<{
+    name: string;
+    color?: string;
+    decorative?: boolean;
+    size?: string;
+  }>(),
+  {
+    decorative: false,
+    size: undefined,
   },
+);
 
-  data() {
-    return {
-      iconSvgData: "",
-    };
-  },
+const iconSvgData = ref(`<svg id="meteor-icon-kit__${props.name}"></svg>`);
 
-  computed: {
-    iconName(): string {
-      return `icons-${this.name}`;
-    },
+const styles = computed(() => {
+  const styles: CSSProperties = {
+    color: props.color,
+  };
 
-    classes(): string[] {
-      const classes = [`icon--${this.name}`];
+  if (props.size) {
+    let size = props.size;
 
-      if (this.size) {
-        classes.push(`mt-icon--custom-size`);
-      }
+    // @ts-expect-error - This is a valid check
+    if (!Number.isNaN(parseFloat(size)) && !Number.isNaN(size - 0)) {
+      size = `${size}px`;
+    }
 
-      return classes;
-    },
+    styles.width = size;
+    styles.height = size;
+  }
 
-    styles(): Record<string, string> {
-      const styles: {
-        color: string;
-        width?: string;
-        height?: string;
-      } = {
-        color: this.color,
-      };
-
-      if (this.size) {
-        let size = this.size;
-
-        // @ts-expect-error - This is a valid check
-        if (!Number.isNaN(parseFloat(size)) && !Number.isNaN(size - 0)) {
-          size = `${size}px`;
-        }
-
-        styles.width = size;
-        styles.height = size;
-      }
-
-      return styles;
-    },
-  },
-
-  watch: {
-    name: {
-      handler(newName) {
-        const [variant] = newName.split("-");
-        const iconName = newName.split("-").slice(1).join("-");
-
-        import(
-          `./../../../../node_modules/@shopware-ag/meteor-icon-kit/icons/${variant}/${iconName}.svg`
-        ).then((iconSvgData) => {
-          if (iconSvgData.default) {
-            this.iconSvgData = iconSvgData.default;
-          } else {
-            console.error(
-              `The SVG file for the icon name ${newName} could not be found and loaded.`,
-            );
-            this.iconSvgData = "";
-          }
-        });
-      },
-      immediate: true,
-    },
-  },
-
-  beforeMount() {
-    this.iconSvgData = `<svg id="meteor-icon-kit__${this.name}"></svg>`;
-  },
+  return styles;
 });
+
+watch(
+  () => props.name,
+  (newName) => {
+    const [variant] = newName.split("-");
+    const iconName = newName.split("-").slice(1).join("-");
+
+    import(
+      `./../../../../node_modules/@shopware-ag/meteor-icon-kit/icons/${variant}/${iconName}.svg`
+    ).then((data) => {
+      if (data.default) {
+        iconSvgData.value = data.default;
+        return;
+      }
+
+      console.error(`The SVG file for the icon name ${newName} could not be found and loaded.`);
+      iconSvgData.value = "";
+    });
+  },
+  { immediate: true },
+);
 </script>
 
 <style lang="scss">
