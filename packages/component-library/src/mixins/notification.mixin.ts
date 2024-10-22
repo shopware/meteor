a96255dce098a9bf6199e95f9f19ac8680ebe315
Nils <haberkamp.n@gmail.com>
2024-10-22 15:38:53 +0200

    get rid of i18n in notification mixin

--- packages/component-library/src/mixins/notification.mixin.ts
+++ packages/component-library/src/mixins/notification.mixin.ts
@@ -1,127 +1,25 @@
 import { defineComponent } from "vue";
 
 export default defineComponent({
-  i18n: {
-    messages: {
-      en: {
-        notification: {
-          success: "Success",
-          info: "Info",
-          warning: "Warning",
-          error: "Error",
-        },
-      },
-      de: {
-        notification: {
-          success: "Erfolg",
-          info: "Info",
-          warning: "Warnung",
-          error: "Fehler",
-        },
-      },
-    },
-  },
-
   methods: {
-    // eslint-disable-next-line no-unused-vars
-    createNotification() {
-      // TODO: Implement a general notification system
-      // return Shopware.State.dispatch('notification/createNotification', notification);
-    },
+    createNotification() {},
 
-    createNotificationSuccess(config: any) {
-      const notification = {
-        variant: "success",
-        title: this.$tc("global.default.success"),
-        ...config,
-      };
+    createNotificationSuccess(config: any) {},
 
-      // @ts-expect-error
-      this.createNotification(notification);
-    },
+    createNotificationInfo(config: any) {},
 
-    createNotificationInfo(config: any) {
-      const notification = {
-        variant: "info",
-        title: this.$tc("global.default.info"),
-        ...config,
-      };
+    createNotificationWarning(config: any) {},
 
-      // @ts-expect-error
-      this.createNotification(notification);
-    },
+    createNotificationError(config: any) {},
 
-    createNotificationWarning(config: any) {
-      const notification = {
-        variant: "warning",
-        title: this.$tc("global.default.warning"),
-        ...config,
-      };
+    createSystemNotificationSuccess(config: any) {},
 
-      // @ts-expect-error
-      this.createNotification(notification);
-    },
+    createSystemNotificationInfo(config: any) {},
 
-    createNotificationError(config: any) {
-      const notification = {
-        variant: "error",
-        title: this.$tc("global.default.error"),
-        ...config,
-      };
+    createSystemNotificationWarning(config: any) {},
 
-      // @ts-expect-error
-      this.createNotification(notification);
-    },
+    createSystemNotificationError(config: any) {},
 
-    createSystemNotificationSuccess(config: any) {
-      const notification = {
-        variant: "success",
-        system: true,
-        ...config,
-      };
-
-      // @ts-expect-error
-      this.createNotification(notification);
-    },
-
-    createSystemNotificationInfo(config: any) {
-      const notification = {
-        variant: "info",
-        system: true,
-        ...config,
-      };
-
-      // @ts-expect-error
-      this.createNotification(notification);
-    },
-
-    createSystemNotificationWarning(config: any) {
-      const notification = {
-        variant: "warning",
-        system: true,
-        ...config,
-      };
-
-      // @ts-expect-error
-      this.createNotification(notification);
-    },
-
-    createSystemNotificationError(config: any) {
-      const notification = {
-        variant: "error",
-        system: true,
-        ...config,
-      };
-
-      // @ts-expect-error
-      this.createNotification(notification);
-    },
-
-    createSystemNotification(config: any) {
-      const notification = { system: true, ...config };
-
-      // @ts-expect-error
-      this.createNotification(notification);
-    },
+    createSystemNotification(config: any) {},
   },
 });
