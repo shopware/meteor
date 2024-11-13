5c308adb9914c4eee95a64b4a2c7649c98810533
Nils <haberkamp.n@gmail.com>
2024-11-13 07:20:57 +0100

    add mt-tooltip component

---
+++ packages/component-library/src/components/overlay/mt-tooltip/mt-tooltip.interactive.stories.ts
@@ -0,0 +1,44 @@
+import { userEvent, within } from "@storybook/test";
+import type { MtTooltipMeta } from "./mt-tooltip.stories";
+import MtTooltipStory from "./mt-tooltip.stories";
+
+export default {
+  ...MtTooltipStory,
+  title: "Interaction Tests/Overlay/mt-tooltip",
+  decorators: [
+    () => ({
+      template: `<div style="height: calc(100vh - 1rem * 2); width: 100%; display: grid; place-items: center;"><story/></div>`,
+    }),
+  ],
+  async play() {
+    await userEvent.tab();
+  },
+} as MtTooltipMeta;
+
+export const VisualTestTooltipTop = {
+  name: "Tooltip is above the trigger",
+  args: {
+    placement: "top",
+  },
+};
+
+export const VisualTestTooltipRight = {
+  name: "Tooltip is to the right of the trigger",
+  args: {
+    placement: "right",
+  },
+};
+
+export const VisualTestTooltipBottom = {
+  name: "Tooltip is below the trigger",
+  args: {
+    placement: "bottom",
+  },
+};
+
+export const VisualTestTooltipLeft = {
+  name: "Tooltip is to the left of the trigger",
+  args: {
+    placement: "left",
+  },
+};
