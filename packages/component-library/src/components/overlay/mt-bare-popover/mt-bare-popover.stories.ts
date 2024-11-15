25de0ddaa641a8cb0e952469d8b8b16e723813cb
Nils <haberkamp.n@gmail.com>
2024-11-15 09:15:53 +0100

    wip

--- packages/component-library/src/components/overlay/mt-bare-popover/mt-bare-popover.stories.ts
+++ packages/component-library/src/components/overlay/mt-bare-popover/mt-bare-popover.stories.ts
@@ -1,6 +1,8 @@
 import type { SlottedMeta } from "@/_internal/story-helper";
 import MtBarePopover from "./mt-bare-popover.vue";
 import MtButton from "../../form/mt-button/mt-button.vue";
+import MtBarePopoverItem from "./sub-components/mt-bare-popover-item.vue";
+import { userEvent, within } from "@storybook/test";
 
 export type MtBarePopoverMeta = SlottedMeta<typeof MtBarePopover, "default" | "trigger">;
 
@@ -8,7 +10,7 @@ export default {
   title: "Components/Overlay/mt-bare-popover",
   render: (args) => ({
     setup: () => args,
-    components: { MtBarePopover, MtButton },
+    components: { MtBarePopover, MtButton, MtBarePopoverItem },
     template: `
 <mt-bare-popover>
     <template #trigger="params">
@@ -16,11 +18,16 @@ export default {
     </template>
 
     <template #default>
-        <div>Content</div>
+        <mt-bare-popover-item />
     </template>
 </mt-bare-popover>
 `,
   }),
+  async play({ canvasElement }) {
+    const canvas = within(canvasElement);
+
+    await userEvent.click(canvas.getByRole("button", { name: "Open popover" }));
+  },
 } satisfies MtBarePopoverMeta;
 
 export const Default = {};
