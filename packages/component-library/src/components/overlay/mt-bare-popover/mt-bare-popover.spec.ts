81c86c23e9d9947e7d73149b02dd3e032d615148
Nils <haberkamp.n@gmail.com>
2024-11-15 07:51:48 +0100

    add toggle to popover

---
+++ packages/component-library/src/components/overlay/mt-bare-popover/mt-bare-popover.spec.ts
@@ -0,0 +1,104 @@
+import { userEvent } from "@storybook/test";
+import { screen, render } from "@testing-library/vue";
+import MtBarePopover from "./mt-bare-popover.vue";
+
+describe("mt-bare-popover", () => {
+  it("does not show the popover by default", () => {
+    // ARRANGE
+    render({
+      components: { MtBarePopover },
+      template: `
+        <mt-bare-popover>
+          <template #trigger="params">
+            <button v-bind="params">Trigger</button>
+          </template>
+
+          <template #default>
+            <div>Content</div>
+          </template>
+        </mt-bare-popover>
+      `,
+    });
+
+    // ASSERT
+    expect(screen.queryByRole("dialog")).not.toBeInTheDocument();
+  });
+
+  it("shows the popover when the trigger is clicked", async () => {
+    // ARRANGE
+    render({
+      components: { MtBarePopover },
+      template: `
+        <mt-bare-popover>
+          <template #trigger="params">
+            <button v-bind="params">Trigger</button>
+          </template>
+
+          <template #default>
+            <div>Content</div>
+          </template>
+        </mt-bare-popover>
+      `,
+    });
+
+    // ACT
+    await userEvent.click(screen.getByRole("button"));
+
+    // ASSERT
+    expect(screen.getByRole("dialog")).toBeVisible();
+  });
+
+  it("hides the popover when the trigger is clicked again", async () => {
+    // ARRANGE
+    render({
+      components: { MtBarePopover },
+      template: `
+        <mt-bare-popover>
+          <template #trigger="params">
+            <button v-bind="params">Trigger</button>
+          </template>
+
+          <template #default>
+            <div>Content</div>
+          </template>
+        </mt-bare-popover>
+      `,
+    });
+
+    await userEvent.click(screen.getByRole("button"));
+
+    // ACT
+    await userEvent.click(screen.getByRole("button"));
+
+    // ASSERT
+    expect(screen.queryByRole("dialog")).not.toBeInTheDocument();
+  });
+
+  it.each(["{Enter}", " "])(
+    'opens the popover when pressing "%s" on the trigger',
+    async (keyCode) => {
+      // ARRANGE
+      render({
+        components: { MtBarePopover },
+        template: `
+        <mt-bare-popover>
+          <template #trigger="params">
+            <button v-bind="params">Trigger</button>
+          </template>
+
+          <template #default>
+            <div>Content</div>
+          </template>
+        </mt-bare-popover>
+      `,
+      });
+
+      // ACT
+      await userEvent.tab();
+      await userEvent.keyboard(keyCode);
+
+      // ASSERT
+      expect(screen.getByRole("dialog")).toBeVisible();
+    },
+  );
+});
