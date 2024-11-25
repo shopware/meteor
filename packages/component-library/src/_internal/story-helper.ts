30b107e7516ed86f43ba7c95ae3abcbd2fe5ea1f
Nils <haberkamp.n@gmail.com>
2024-11-25 11:15:36 +0100

    make defining stories easier

--- packages/component-library/src/_internal/story-helper.ts
+++ packages/component-library/src/_internal/story-helper.ts
@@ -1,7 +1,44 @@
 import type { Meta } from "@storybook/vue3";
 import type { DefineComponent } from "vue";
+import { within } from "@storybook/test";
+import type { ComponentPropsAndSlots, StoryObj, VueRenderer } from "@storybook/vue3";
+import type { BoundFunctions, queries } from "@testing-library/vue";
+import type { FunctionalComponent } from "vue";
+import type { PlayFunctionContext } from "@storybook/types";
+import type { Constructor } from "type-fest";
 
 export type SlottedMeta<
   TComponent extends abstract new (...args: any) => any,
   TSlots extends string,
 > = Meta<DefineComponent<InstanceType<TComponent>["$props"] & Record<TSlots, any>>>;
+
+type ComponentPropsOrProps<TCmpOrArgs> =
+  TCmpOrArgs extends Constructor<any>
+    ? ComponentPropsAndSlots<TCmpOrArgs>
+    : TCmpOrArgs extends FunctionalComponent<any>
+      ? ComponentPropsAndSlots<TCmpOrArgs>
+      : TCmpOrArgs;
+
+export function defineStory<T>(
+  config: Omit<StoryObj<T>, "play"> & {
+    play?: (
+      context: Omit<StoryObj<T>, "play"> & { screen: BoundFunctions<typeof queries> },
+    ) => Promise<void>;
+  },
+  extension?: { from: StoryObj<T> },
+): StoryObj<T> {
+  if (!config.play) {
+    return config as StoryObj<T>;
+  }
+
+  return {
+    ...extension?.from,
+    ...config,
+    play: async (context: PlayFunctionContext<VueRenderer, ComponentPropsOrProps<T>>) => {
+      if (!config.play) return () => void 0;
+
+      const screen = within(document.body);
+      await config.play({ ...context, screen });
+    },
+  } as StoryObj<T>;
+}
