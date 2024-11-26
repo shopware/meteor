a8d2d83b9c27e6f95961cefb0dea23e8d321147c
alastair-simon <alastairfraser.work@gmail.com>
2024-11-26 18:23:57 +0100

    replace mt-external-link with mt-link for mt-empty-state

--- packages/component-library/src/components/layout/mt-empty-state/mt-empty-state.vue
+++ packages/component-library/src/components/layout/mt-empty-state/mt-empty-state.vue
@@ -12,6 +12,16 @@
       {{ description }}
     </mt-text>
 
+    <mt-link
+      v-if="linkHref && linkText"
+      :href="linkHref"
+      class="mt-empty-state__link"
+      type="external"
+      as="a"
+    >
+      {{ linkText }}
+    </mt-link>
+
     <mt-button
       v-if="buttonText"
       class="mt-empty-state__button"
@@ -29,6 +39,7 @@
 import MtButton from "@/components/form/mt-button/mt-button.vue";
 import MtIcon from "../../icons-media/mt-icon/mt-icon.vue";
 import MtText from "@/components/content/mt-text/mt-text.vue";
+import MtLink from "@/components/navigation/mt-link/mt-link.vue";
 
 defineProps<{
   headline: string;
@@ -43,24 +54,35 @@ defineEmits(["button-click"]);
 </script>
 
 <style scoped>
+.mt-empty-state {
+  display: grid;
+}
+
 .mt-empty-state__icon {
   display: inline-block;
   padding: 0.75rem;
   border-radius: var(--border-radius-xs);
   background-color: var(--color-interaction-secondary-dark);
-  margin-block-end: 1.5rem;
+  justify-self: start;
 }
 
 .mt-empty-state__headline {
-  margin-block-end: 0.5rem;
+  margin-top: 1.5rem;
+  justify-self: start;
+  margin-bottom: 0;
+}
+
+.mt-empty-state__description {
+  justify-self: start;
+  margin-top: 0.5rem;
 }
 
 .mt-empty-state__link {
-  margin-block-start: 0.5rem;
-  display: block;
+  margin-top: 0.5rem;
 }
 
 .mt-empty-state__button {
-  margin-block-start: 1.5rem;
+  justify-self: start;
+  margin-top: 1.5rem;
 }
 </style>
