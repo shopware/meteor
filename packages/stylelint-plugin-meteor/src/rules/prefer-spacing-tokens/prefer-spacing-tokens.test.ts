220202024e86adf3dfc5812c7ca5d3f159c27731
Nils <haberkamp.n@gmail.com>
2024-11-27 11:11:23 +0100

    feat: lint lines where rem values are used

--- packages/stylelint-plugin-meteor/src/rules/prefer-spacing-tokens/prefer-spacing-tokens.test.ts
+++ packages/stylelint-plugin-meteor/src/rules/prefer-spacing-tokens/prefer-spacing-tokens.test.ts
@@ -39,5 +39,14 @@ testRule({
       endLine: 1,
       endColumn: 19,
     },
+    {
+      code: ".a { margin: 1rem; }",
+      message:
+        'Unexpected hard-coded spacing of "1rem" (meteor/prefer-spacing-token)',
+      line: 1,
+      column: 6,
+      endLine: 1,
+      endColumn: 19,
+    },
   ],
 });
