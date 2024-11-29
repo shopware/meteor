c05ab87a8e053ee1b71fc3a8146f3f7cf9088ed2
Nils <haberkamp.n@gmail.com>
2024-11-29 07:46:34 +0100

    chore: supress typescript error

--- packages/stylelint-plugin-meteor/src/rules/prefer-sizing-token/prefer-spacing-token.test.ts
+++ packages/stylelint-plugin-meteor/src/rules/prefer-sizing-token/prefer-spacing-token.test.ts
@@ -3,6 +3,7 @@ import { testRule } from "stylelint-test-rule-node";
 import plugin from "./index.js";
 
 const {
+  // @ts-expect-error - Cannot infer type correctly
   rule: { ruleName },
 } = plugin;
 
