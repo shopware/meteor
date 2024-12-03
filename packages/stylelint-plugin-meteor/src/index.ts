bedb94382aad0e8b173be1e77b9dda66bfac2e6e
Nils <haberkamp.n@gmail.com>
2024-12-03 08:42:54 +0100

    feat: add meteor/prefer-color-token rule

--- packages/stylelint-plugin-meteor/src/index.ts
+++ packages/stylelint-plugin-meteor/src/index.ts
@@ -1,4 +1,5 @@
 import preferSizingTokens from "./rules/prefer-sizing-token/index.js";
 import preferBackgroundToken from "./rules/prefer-background-token/index.js";
+import preferColorToken from "./prefer-color-token/index.js";
 
-export default [preferSizingTokens, preferBackgroundToken];
+export default [preferSizingTokens, preferColorToken, preferBackgroundToken];
