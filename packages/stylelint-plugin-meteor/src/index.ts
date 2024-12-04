02ecfaffc5aead6267294de57f08b145adad8d72
Nils <haberkamp.n@gmail.com>
2024-12-04 08:06:25 +0100

    feat(stylelint-plugin-meteor): add no primitive token rule

--- packages/stylelint-plugin-meteor/src/index.ts
+++ packages/stylelint-plugin-meteor/src/index.ts
@@ -1,5 +1,11 @@
 import preferSizingTokens from "./rules/prefer-sizing-token/index.js";
 import preferBackgroundToken from "./rules/prefer-background-token/index.js";
 import preferColorToken from "./prefer-color-token/index.js";
+import noPrimitiveToken from "./rules/no-primitive-token/index.js";
 
-export default [preferSizingTokens, preferColorToken, preferBackgroundToken];
+export default [
+  preferSizingTokens,
+  preferColorToken,
+  preferBackgroundToken,
+  noPrimitiveToken,
+];
