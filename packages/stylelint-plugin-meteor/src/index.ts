6bb28ee55b0add7a0ac2be7b08a19a8db375db1e
Nils <haberkamp.n@gmail.com>
2024-12-06 09:16:21 +0000

    feat(stylelint-plugin-meteor): add types for rules

--- packages/stylelint-plugin-meteor/src/index.ts
+++ packages/stylelint-plugin-meteor/src/index.ts
@@ -4,6 +4,7 @@ import preferColorToken from "./prefer-color-token/index.js";
 import noPrimitiveToken from "./rules/no-primitive-token/index.js";
 import preferFontToken from "./rules/prefer-font-token/index.js";
 import preferBorderToken from "./rules/prefer-border-token/index.js";
+import "./types.mjs";
 
 export default [
   preferSizingTokens,
@@ -13,4 +14,3 @@ export default [
   noPrimitiveToken,
   preferFontToken,
 ];
-
