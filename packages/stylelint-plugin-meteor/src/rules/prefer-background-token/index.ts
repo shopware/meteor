542dfddc2e276e7669576f75fe9215c8817dc1c8
Nils <haberkamp.n@gmail.com>
2024-12-06 09:14:02 +0000

    docs(stylelint-plugin-meteor): add documentation for meteor/prefer-background-token rule

--- packages/stylelint-plugin-meteor/src/rules/prefer-background-token/index.ts
+++ packages/stylelint-plugin-meteor/src/rules/prefer-background-token/index.ts
@@ -15,7 +15,7 @@ const messages = ruleMessages(ruleName, {
 });
 
 const meta = {
-  url: "",
+  url: "https://github.com/shopware/meteor/blob/main/packages/stylelint-plugin-meteor/src/rules/prefer-background-token/README.md",
 };
 
 const ruleFunction: Rule = (primary) => {
