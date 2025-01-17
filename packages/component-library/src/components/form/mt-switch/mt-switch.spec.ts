971d3fc810e6b94994d4e5ce0bf59557ccf84fda
alastair-simon <alastairfraser.work@gmail.com>
2025-01-17 17:55:17 +0100

    test: add tests for mt-switch

---
+++ packages/component-library/src/components/form/mt-switch/mt-switch.spec.ts
@@ -0,0 +1,41 @@
+import { mount } from "@vue/test-utils";
+import MtSwitch from "./mt-switch.vue";
+
+async function createWrapper(props = {}, options = {}) {
+  return mount(MtSwitch, {
+    propsData: {
+      label: "Test Switch",
+      ...props,
+    },
+    ...options,
+  });
+}
+
+describe("mt-switch", () => {
+  let wrapper: Awaited<ReturnType<typeof createWrapper>>;
+
+  afterEach(() => {
+    wrapper?.unmount();
+  });
+
+  it("should update checked state when checked", async () => {
+    wrapper = await createWrapper();
+    const checkbox = wrapper?.find('input[type="checkbox"]');
+    await wrapper?.setProps({ checked: true });
+    expect(checkbox?.element).toBeChecked();
+    await wrapper?.setProps({ checked: false });
+    expect(checkbox?.element).not.toBeChecked();
+  });
+
+  it("should have correct ARIA attributes", async () => {
+    wrapper = await createWrapper({
+      checked: true,
+      label: "Accessibility Test",
+    });
+    const checkbox = wrapper.find('input[type="checkbox"]');
+
+    expect(checkbox.attributes("role")).toBe("checkbox");
+    expect(checkbox.attributes("aria-checked")).toBe("true");
+    expect(checkbox.attributes("aria-label")).toBe("Accessibility Test");
+  });
+});
