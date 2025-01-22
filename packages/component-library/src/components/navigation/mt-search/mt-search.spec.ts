e35642e7bbe2e88baeee8e2aba07d1ed638a69dd
Nils <haberkamp.n@gmail.com>
2025-01-22 10:10:07 +0100

    test(component-library): removing focus from the input emits a change vent

--- packages/component-library/src/components/navigation/mt-search/mt-search.spec.ts
+++ packages/component-library/src/components/navigation/mt-search/mt-search.spec.ts
@@ -22,6 +22,47 @@ describe("mt-search", () => {
     expect(handler).toHaveBeenCalledWith("Hello");
   });
 
+  it("emits a change event when typing in the search and then removing the focus from the input", async () => {
+    // ARRANGE
+    const handler = vi.fn();
+
+    render(MtSearch, {
+      props: {
+        modelValue: "",
+        onChange: handler,
+      },
+    });
+
+    // ACT
+    await userEvent.type(screen.getByRole("searchbox"), "Hello");
+    await userEvent.tab();
+
+    // ASSERT
+    expect(screen.getByRole("searchbox")).toHaveValue("Hello");
+
+    expect(handler).toHaveBeenCalledWith("Hello");
+  });
+
+  it("emits an empty string when clearing the search and then removing focus from the input", async () => {
+    // ARRANGE
+    const handler = vi.fn();
+
+    render(MtSearch, {
+      props: {
+        modelValue: "Hello",
+        onChange: handler,
+      },
+    });
+
+    // ACT
+    await userEvent.clear(screen.getByRole("searchbox"));
+    await userEvent.tab();
+
+    // ASSERT
+    expect(screen.getByRole("searchbox")).toHaveValue("");
+    expect(handler).toHaveBeenCalledWith("");
+  });
+
   it("has a default placeholder of search", async () => {
     // ARRANGE
     render(MtSearch);
