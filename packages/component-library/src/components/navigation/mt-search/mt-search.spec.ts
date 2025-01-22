2ea6de6c231236470cab13cea475e1783a9f0178
Nils <haberkamp.n@gmail.com>
2025-01-22 10:04:09 +0100

    test(component-library): search can be disabled

--- packages/component-library/src/components/navigation/mt-search/mt-search.spec.ts
+++ packages/component-library/src/components/navigation/mt-search/mt-search.spec.ts
@@ -1,5 +1,6 @@
 import { render, screen } from "@testing-library/vue";
 import MtSearch from "./mt-search.vue";
+import { userEvent } from "@storybook/test";
 
 describe("mt-search", () => {
   it("has a default placeholder of search", async () => {
@@ -20,5 +21,37 @@ describe("mt-search", () => {
 
     // ASSERT
     expect(screen.getByRole("searchbox")).toHaveAttribute("placeholder", "Find me");
+  });
+
+  it("disables the search", async () => {
+    // ARRANGE
+    render(MtSearch, {
+      props: {
+        disabled: true,
+      },
+    });
+
+    // ASSERT
+    expect(screen.getByRole("searchbox")).toBeDisabled();
+  });
+
+  it("does not change the value when the search is disabled", async () => {
+    // ARRANGE
+    const handler = vi.fn();
+
+    render(MtSearch, {
+      props: {
+        modelValue: "Hello",
+        disabled: true,
+        "onUpdate:modelValue": handler,
+      },
+    });
+
+    // ACT
+    await userEvent.type(screen.getByRole("searchbox"), "World");
+
+    // ASSERT
+    expect(screen.getByRole("searchbox")).toHaveValue("Hello");
+    expect(handler).not.toHaveBeenCalled();
   });
 });
