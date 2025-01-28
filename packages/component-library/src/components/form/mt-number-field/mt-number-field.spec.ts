90240d65d53d28633e2c6a3911bd6d43467982d9
Nils <haberkamp.n@gmail.com>
2025-01-28 07:47:10 +0100

    test(component-library): increment by one step when in integer mode

--- packages/component-library/src/components/form/mt-number-field/mt-number-field.spec.ts
+++ packages/component-library/src/components/form/mt-number-field/mt-number-field.spec.ts
@@ -3,6 +3,30 @@ import MtNumberField from "./mt-number-field.vue";
 import { userEvent } from "@storybook/test";
 
 describe("mt-number-field", () => {
+  it("increases the number by default by 1 step when in integer mode", async () => {
+    // ARRANGE
+    const handler = vi.fn();
+
+    render(MtNumberField, {
+      props: {
+        modelValue: 0,
+        numberType: "int",
+        // @ts-expect-error -- Event exist, but type is not defined via TypeScript
+        onChange: handler,
+      },
+    });
+
+    // ACT
+    await userEvent.click(screen.getByRole("textbox"));
+    await userEvent.keyboard("{ArrowUp}");
+
+    await userEvent.tab();
+
+    // ASSERT
+    expect(screen.getByRole("textbox")).toHaveValue("1");
+    expect(handler).toHaveBeenCalledWith(1);
+  });
+
   it("increases the number by 5 steps", async () => {
     // ARRANGE
     const handler = vi.fn();
