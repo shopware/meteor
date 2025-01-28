b3a7443a985e063c2047add50d408b4e44ac7b51
Nils <haberkamp.n@gmail.com>
2025-01-28 13:39:02 +0100

    chore(component-library): make decrement by 5 test pass

--- packages/component-library/src/components/form/mt-number-field/mt-number-field.spec.ts
+++ packages/component-library/src/components/form/mt-number-field/mt-number-field.spec.ts
@@ -436,13 +436,10 @@ describe("mt-number-field", () => {
     });
 
     // ACT
-    await userEvent.click(screen.getByRole("textbox"));
-    await userEvent.keyboard("{ArrowDown}");
-
-    await userEvent.tab();
+    await userEvent.click(screen.getByRole("button", { name: "Decrease" }));
 
     // ASSERT
-    expect(screen.getByRole("textbox")).toHaveValue("-5");
+    expect(screen.getByRole("spinbutton")).toHaveValue(-5);
     expect(handler).toHaveBeenCalledWith(-5);
   });
 
