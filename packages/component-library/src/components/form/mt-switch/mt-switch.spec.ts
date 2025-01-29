3c4f746ee04adac94e0ee1f49434b377ba9844c3
Nils <haberkamp.n@gmail.com>
2025-01-29 11:05:18 +0100

    test(component-library): user can turn of the switch

--- packages/component-library/src/components/form/mt-switch/mt-switch.spec.ts
+++ packages/component-library/src/components/form/mt-switch/mt-switch.spec.ts
@@ -1,3 +1,4 @@
+import { userEvent } from "@storybook/test";
 import MtSwitch from "./mt-switch.vue";
 import { render, screen } from "@testing-library/vue";
 
@@ -24,5 +25,27 @@ describe("mt-switch", () => {
 
     // ASSERT
     expect(screen.getByRole("checkbox")).not.toBeChecked();
+  });
+
+  it("emits an event when turning off the switch", async () => {
+    // ARRANGE
+    const handler = vi.fn();
+
+    render(MtSwitch, {
+      props: {
+        checked: true,
+        // @ts-expect-error -- Type is not defined because of missing defineEmits
+        onChange: handler,
+      },
+    });
+
+    // ACT
+    await userEvent.click(screen.getByRole("checkbox"));
+
+    // ASSERT
+    expect(screen.getByRole("checkbox")).not.toBeChecked();
+
+    expect(handler).toHaveBeenCalledOnce();
+    expect(handler).toHaveBeenCalledWith(false);
   });
 });
