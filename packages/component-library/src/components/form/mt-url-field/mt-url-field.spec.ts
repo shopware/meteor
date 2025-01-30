1a31c42f64028b8851c28dccc54fe625bca98784
Nils <haberkamp.n@gmail.com>
2025-01-30 15:22:16 +0100

    test(component-library): let the user update the url

--- packages/component-library/src/components/form/mt-url-field/mt-url-field.spec.ts
+++ packages/component-library/src/components/form/mt-url-field/mt-url-field.spec.ts
@@ -1,6 +1,7 @@
 import { describe, expect, it } from "vitest";
 import MtUrlField from "./mt-url-field.vue";
 import { render, screen } from "@testing-library/vue";
+import userEvent from "@testing-library/user-event";
 
 describe("mt-url-field", async () => {
   it("shows the domain to the user", async () => {
@@ -25,5 +26,24 @@ describe("mt-url-field", async () => {
 
     // ASSERT
     expect(screen.getByRole("textbox")).toHaveValue("www.example.com");
+  });
+
+  it("updates the domain when the use types", async () => {
+    // ARRANGE
+    const handler = vi.fn();
+    render(MtUrlField, {
+      props: {
+        modelValue: "",
+        // @ts-expect-error -- Event is not typed, yet
+        "onUpdate:modelValue": handler,
+      },
+    });
+
+    // ACT
+    await userEvent.type(screen.getByRole("textbox"), "www.shopware.com");
+
+    // ASSERT
+    expect(screen.getByRole("textbox")).toHaveValue("www.shopware.com");
+    expect(handler).toHaveBeenCalledTimes(1);
   });
 });
