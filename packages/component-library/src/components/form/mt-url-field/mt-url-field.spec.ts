c662629ba6a8061102797dc24bf6ea0850136639
Nils <haberkamp.n@gmail.com>
2025-01-30 15:16:31 +0100

    test(component-library): show url without protocol

---
+++ packages/component-library/src/components/form/mt-url-field/mt-url-field.spec.ts
@@ -0,0 +1,17 @@
+import { describe, expect, it } from "vitest";
+import MtUrlField from "./mt-url-field.vue";
+import { render, screen } from "@testing-library/vue";
+
+describe("mt-url-field", async () => {
+  it("shows the domain without the protocol to the user", async () => {
+    // ARRANGE
+    render(MtUrlField, {
+      props: {
+        modelValue: "https://www.example.com",
+      },
+    });
+
+    // ASSERT
+    expect(screen.getByRole("textbox")).toHaveValue("www.example.com");
+  });
+});
