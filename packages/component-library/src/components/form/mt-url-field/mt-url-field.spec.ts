a30a523bd8e9ba5fb90559ce140384b7e6e40065
Nils <haberkamp.n@gmail.com>
2025-01-30 15:18:34 +0100

    test(component-library): show the domain to the user

--- packages/component-library/src/components/form/mt-url-field/mt-url-field.spec.ts
+++ packages/component-library/src/components/form/mt-url-field/mt-url-field.spec.ts
@@ -3,6 +3,18 @@ import MtUrlField from "./mt-url-field.vue";
 import { render, screen } from "@testing-library/vue";
 
 describe("mt-url-field", async () => {
+  it("shows the domain to the user", async () => {
+    // ARRANGE
+    render(MtUrlField, {
+      props: {
+        modelValue: "www.example.com",
+      },
+    });
+
+    // ASSERT
+    expect(screen.getByRole("textbox")).toHaveValue("www.example.com");
+  });
+
   it("shows the domain without the protocol to the user", async () => {
     // ARRANGE
     render(MtUrlField, {
