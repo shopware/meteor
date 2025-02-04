25ee92d8cbc4e88757ff7e4cac6ade02b84e15b6
Nils <haberkamp.n@gmail.com>
2025-02-04 13:07:45 +0100

    refactor(component-library): get rid of mt-text-field in mt-url-field

--- packages/component-library/src/components/form/mt-url-field/mt-url-field.interactive.stories.ts
+++ packages/component-library/src/components/form/mt-url-field/mt-url-field.interactive.stories.ts
@@ -1,4 +1,4 @@
-import { userEvent, within } from "@storybook/test";
+import { fn, userEvent, within, expect } from "@storybook/test";
 import meta, { type MtUrlFieldMeta, type MtUrlFieldStory } from "./mt-url-field.stories";
 
 export default {
@@ -15,6 +15,20 @@ export const VisualTestFocused: MtUrlFieldStory = {
     const canvas = within(canvasElement);
 
     await userEvent.click(canvas.getByRole("textbox"));
+  },
+};
+
+export const VisualTestPlaceholder: MtUrlFieldStory = {
+  name: "With placeholder",
+  args: {
+    placeholder: "Placeholder",
+  },
+};
+
+export const VisualTestRequired: MtUrlFieldStory = {
+  name: "Required",
+  args: {
+    required: true,
   },
 };
 
@@ -81,5 +95,39 @@ export const VisualTestSmall: MtUrlFieldStory = {
   args: {
     modelValue: "https://example.com",
     size: "small",
+  },
+};
+
+export const VisualTestHelpText: MtUrlFieldStory = {
+  name: "Helptext",
+  args: {
+    modelValue: "https://example.com",
+    helpText: "This is help text",
+  },
+};
+
+export const VisualTestHint: MtUrlFieldStory = {
+  name: "Hint",
+  args: {
+    hint: "This is a hint",
+  },
+};
+
+export const VisualTestCopyToClipboard: MtUrlFieldStory = {
+  name: "Copy to clipboard",
+  args: {
+    modelValue: "https://www.example.com",
+    copyable: true,
+  },
+  async play({ canvasElement }) {
+    const handler = fn();
+    navigator.clipboard.writeText = handler;
+
+    const canvas = within(canvasElement);
+
+    await userEvent.click(canvas.getByRole("button", { name: "Copy URL to clipboard" }));
+
+    await expect(handler).toHaveBeenCalledOnce();
+    await expect(handler).toHaveBeenCalledWith("www.example.com");
   },
 };
