2fbc665dbe5ede0004284e1acae7e24b45da7cd5
Nils <haberkamp.n@gmail.com>
2025-02-18 13:15:53 +0100

    fix(component-library): stop selecting text when double clicking on a label

--- packages/component-library/src/components/form/_internal/mt-field-label/mt-field-label.vue
+++ packages/component-library/src/components/form/_internal/mt-field-label/mt-field-label.vue
@@ -1,5 +1,18 @@
 <template>
-  <label :for="id" :class="classes">
+  <label
+    :for="id"
+    :class="classes"
+    @mousedown="
+      (event) => {
+        // only prevent text selection if clicking inside the label itself
+        const target = event.target as HTMLElement;
+        if (target.closest('button, input, select, textarea')) return;
+
+        // prevent text selection when double clicking label
+        if (!event.defaultPrevented && event.detail > 1) event.preventDefault();
+      }
+    "
+  >
     <button
       v-if="inheritance !== 'none'"
       class="mt-field-label__inheritance-switch"
