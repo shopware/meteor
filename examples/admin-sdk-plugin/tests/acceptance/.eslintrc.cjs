26a5211d6b6be82d399511c195db3765167f7082
Nils <haberkamp.n@gmail.com>
2025-02-21 14:01:20 +0100

    build(admin-sdk-plugin): disable all stylistic lint rules
    The job of a a linter is not to format code, that's the job
    of an formatter like prettier or BiomeJS.
    
    ESLint even removed all stylistic linting rules in v9,
    so this change is a preperation for the ESLint v9 migration.

--- examples/admin-sdk-plugin/tests/acceptance/.eslintrc.cjs
+++ examples/admin-sdk-plugin/tests/acceptance/.eslintrc.cjs
@@ -2,13 +2,13 @@ module.exports = {
     extends: [
         "eslint:recommended",
         "plugin:@typescript-eslint/recommended",
-        "plugin:playwright/recommended"
+        "plugin:playwright/recommended",
+        "prettier"
     ],
     parser: "@typescript-eslint/parser",
     plugins: ["@typescript-eslint"],
     root: true,
     rules: {
-        quotes: ["error", "single", { allowTemplateLiterals: true }],
         "no-console": ["error", { allow: ["warn", "error"] }],
         "comma-dangle": ["error", "always-multiline"],
         "no-unused-vars": "warn",
