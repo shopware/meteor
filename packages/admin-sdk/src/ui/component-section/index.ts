9aa040626f0fa4fa2dfd90344832b5caba222739
Emre Imamoglu <e.imamoglu@shopware.com>
2025-02-26 16:16:49 +0100

    feat: add type support for uiComponentSectionRenderer

--- packages/admin-sdk/src/ui/component-section/index.ts
+++ packages/admin-sdk/src/ui/component-section/index.ts
@@ -2,39 +2,34 @@ import { createSender } from '../../channel';
 import { getWindowSrc } from '../../_internals/utils';
 
 export const add = createSender('uiComponentSectionRenderer', {
-  src: getWindowSrc() ?? undefined,
+    src: getWindowSrc() ?? undefined,
 });
 
-/**
- * Contains all possible components for the sections
- */
-export type uiComponentSectionRenderer =
- {
-   responseType: void,
-   component: string,
-   positionId: string,
-   props: unknown,
-   src?: string,
- } &
- (
-   cardComponentRender
-   /**
-    * Here you can add multiple component types. Add with "|" so that only one will get used.
-    */
-)
-
-interface cardComponentRender {
-  component: 'card',
-  props: {
-    title?: string,
-    subtitle?: string,
-    locationId: string,
-    tabs?: cardTabRenderer[],
-  },
+interface BaseSectionRenderer {
+    responseType: void;
+    positionId: string;
+    src?: string;
 }
 
-interface cardTabRenderer {
-  name: string,
-  label: string,
-  locationId: string,
+interface CardComponentRender extends BaseSectionRenderer {
+    component: 'card';
+    props: {
+        title?: string;
+        subtitle?: string;
+        locationId: string;
+        tabs?: CardTabRenderer[];
+    };
 }
+
+interface CardTabRenderer {
+    name: string;
+    label: string;
+    locationId: string;
+}
+
+interface HtmlElementRender extends BaseSectionRenderer {
+    component: keyof HTMLElementTagNameMap;
+    props: Record<string, unknown>;
+}
+
+export type uiComponentSectionRenderer = CardComponentRender | HtmlElementRender;
