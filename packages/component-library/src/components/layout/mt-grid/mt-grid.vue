838a4812dc96e69f8bc8861418990c0f814bf56b
Nils <haberkamp.n@gmail.com>
2025-02-26 09:31:13 +0100

    refactor(component-library): add variable for grid gap

--- packages/component-library/src/components/layout/mt-grid/mt-grid.vue
+++ packages/component-library/src/components/layout/mt-grid/mt-grid.vue
@@ -22,6 +22,6 @@ defineSlots<{
 
   gap: var(--_mt-grid-gap);
   display: grid;
-  grid-template-columns: repeat(auto-fit, minmax(max(calc(33% - 32px), 300px), 1fr));
+  grid-template-columns: repeat(auto-fit, minmax(max(calc(33% - var(--_mt-grid-gap)), 300px), 1fr));
 }
 </style>
