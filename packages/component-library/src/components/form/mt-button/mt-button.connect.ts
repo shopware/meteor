ac7aa66d3d54a11cd6dea76141f9a97dae5abc15
Nils <haberkamp.n@gmail.com>
2025-03-05 09:52:39 +0000

    docs(component-library): define code connect for primary button

---
+++ packages/component-library/src/components/form/mt-button/mt-button.connect.ts
@@ -0,0 +1,19 @@
+import figma, { html } from "@figma/code-connect/html";
+
+figma.connect(
+  "https://www.figma.com/design/Krub3xbG0vMUdoL8vzEZ7l/%E2%98%84%EF%B8%8F-Meteor-Admin-Foundations-%E2%80%93-6.0.0?node-id=6793-395952",
+  {
+    props: {
+      text: figma.textContent("Button text"),
+      size: figma.enum("size", {
+        Default: "default",
+        Small: "small",
+      }),
+      disabled: figma.boolean("disabled"),
+      ghost: figma.boolean("ghost"),
+    },
+    example: ({ text, size, disabled, ghost }) =>
+      html`<mt-button variant="primary" ghost="${ghost}" size="${size}" disabled="${disabled}" @click="() => {}">${text}</mt-button>`,
+    imports: ["import { MtButton } from '@shopware-ag/meteor-component-library';"],
+  },
+);
