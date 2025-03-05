0aad0d47647e8f16be889f3ae12849adf173e4bc
Nils <haberkamp.n@gmail.com>
2025-03-05 12:38:08 +0000

    docs(component-library): add Code Connect for secondary button

--- packages/component-library/src/components/form/mt-button/mt-button.connect.ts
+++ packages/component-library/src/components/form/mt-button/mt-button.connect.ts
@@ -24,3 +24,22 @@ figma.connect(
     imports: ["import { MtButton } from '@shopware-ag/meteor-component-library';"],
   },
 );
+
+figma.connect(
+  "https://www.figma.com/design/Krub3xbG0vMUdoL8vzEZ7l/%E2%98%84%EF%B8%8F-Meteor-Admin-Foundations-%E2%80%93-6.0.0?node-id=6793-396743",
+  {
+    props: {
+      text: figma.textContent("Button text"),
+      size: figma.enum("size", {
+        Default: "default",
+        Small: "small",
+      }),
+      disabled: figma.boolean("disabled"),
+    },
+    example: ({ text, size, disabled }) =>
+      html`<mt-button variant="secondary" size="${size}" disabled="${disabled}" @click="() => {}"
+        >${text}</mt-button
+      >`,
+    imports: ["import { MtButton } from '@shopware-ag/meteor-component-library';"],
+  },
+);
