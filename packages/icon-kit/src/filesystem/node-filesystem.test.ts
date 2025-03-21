450c5fc02b7311076c132f39cbc26ab0cda089b8
Nils <haberkamp.n@gmail.com>
2025-03-21 15:12:41 +0100

    chore(icon-kit): log writing to file system

--- packages/icon-kit/src/filesystem/node-filesystem.test.ts
+++ packages/icon-kit/src/filesystem/node-filesystem.test.ts
@@ -1,6 +1,7 @@
 import { test, expect, vi } from "vitest";
 import { NodeFilesystem } from "./node-filesystem.js";
 import fs from "node:fs";
+import { NoOpLogger } from "../logger/no-op-logger.js";
 
 vi.mock("node:fs", () => {
   return {
@@ -15,7 +16,7 @@ test("creates a file", () => {
   // ARRANGE
   const spy = vi.spyOn(fs, "writeFileSync");
 
-  const subject = new NodeFilesystem();
+  const subject = new NodeFilesystem(new NoOpLogger());
 
   // ACT
   subject.createFile("./foo.txt", "Hello, world");
@@ -26,7 +27,7 @@ test("creates a file", () => {
 
 test("reads a file", () => {
   // ARRANGE
-  const subject = new NodeFilesystem();
+  const subject = new NodeFilesystem(new NoOpLogger());
   vi.spyOn(fs, "readFileSync").mockReturnValue("Hello, world");
 
   // ACT
@@ -38,7 +39,7 @@ test("reads a file", () => {
 
 test("returns undefined when reading a non-existing file", () => {
   // ARRANGE
-  const subject = new NodeFilesystem();
+  const subject = new NodeFilesystem(new NoOpLogger());
   // @ts-expect-error -- fix later
   vi.spyOn(fs, "readFileSync").mockReturnValue(undefined);
 
--- packages/icon-kit/src/filesystem/node-filesystem.ts
+++ packages/icon-kit/src/filesystem/node-filesystem.ts
@@ -1,9 +1,14 @@
+import type { Logger } from "../logger/logger.js";
 import type { Filesystem } from "./filesystem.js";
 import fs from "node:fs";
 
 export class NodeFilesystem implements Filesystem {
+  constructor(private readonly logger: Logger) {}
+
   createFile(path: string, content: string): void {
     fs.writeFileSync(path, content);
+
+    this.logger.info(`Create file: ${path}`);
   }
 
   readFile(path: string): string {
