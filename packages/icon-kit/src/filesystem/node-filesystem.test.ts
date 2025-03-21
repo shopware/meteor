fd91dda354f779bdbe312756b6e069d4e6751404
Nils <haberkamp.n@gmail.com>
2025-03-21 11:31:17 +0100

    chore(icon-kit): add filesystem

---
+++ packages/icon-kit/src/filesystem/node-filesystem.test.ts
@@ -0,0 +1,37 @@
+import { test, expect, vi } from "vitest";
+import { NodeFilesystem } from "./node-filesystem.js";
+import fs from "node:fs";
+
+vi.mock("node:fs", () => {
+  return {
+    default: {
+      writeFileSync: () => null,
+      readFileSync: () => null,
+    },
+  };
+});
+
+test("creates a file", () => {
+  // ARRANGE
+  const spy = vi.spyOn(fs, "writeFileSync");
+
+  const subject = new NodeFilesystem();
+
+  // ACT
+  subject.createFile("./foo.txt", "Hello, world");
+
+  // ASSERT
+  expect(spy).toHaveBeenCalledExactlyOnceWith("./foo.txt", "Hello, world");
+});
+
+test("reads a file", () => {
+  // ARRANGE
+  const subject = new NodeFilesystem();
+  vi.spyOn(fs, "readFileSync").mockReturnValue("Hello, world");
+
+  // ACT
+  const result = subject.readFile("./foo.txt");
+
+  // ASSERT
+  expect(result).toBe("Hello, world");
+});
