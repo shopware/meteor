b0d1f057a9e603a0ddf5896472afd57141966c1f
alastair-simon <alastairfraser.work@gmail.com>
2025-04-14 10:21:16 +0200

    fix: set value after clear null
    test: add test to verify clear null value
    
    fix: add changeset
    
    fix: visual test to reflect null value
    
    fix: allow empty array return if multiSelect enabled
    
    fix: visual clear test
    
    test: add additional unit test
    
    refactor: simplify logic

--- packages/component-library/src/components/form/mt-select/mt-select.vue
+++ packages/component-library/src/components/form/mt-select/mt-select.vue
@@ -400,9 +400,8 @@ export default defineComponent({
     currentValue: {
       get(): string | number | boolean | unknown[] | null | undefined {
         if (this.modelValue === null || this.modelValue === undefined) {
-          return [];
+          return this.enableMultiSelection ? [] : null;
         }
-
         return this.modelValue;
       },
       set(newValue: string | number | boolean | unknown[] | null | undefined) {
@@ -565,7 +564,8 @@ export default defineComponent({
     },
 
     onClearSelection() {
-      this.currentValue = [];
+      // If multiSelection enabled return empty array otherwise null
+      this.currentValue = this.enableMultiSelection ? [] : null;
     },
 
     getFocusElement() {
