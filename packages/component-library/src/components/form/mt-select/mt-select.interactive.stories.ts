506727f48d2c5f8c6cbd5e37f395eb02e59577c6
alastair-simon <alastairfraser.work@gmail.com>
2025-04-14 13:52:12 +0200

    fix: visual test to reflect null value

--- packages/component-library/src/components/form/mt-select/mt-select.interactive.stories.ts
+++ packages/component-library/src/components/form/mt-select/mt-select.interactive.stories.ts
@@ -55,7 +55,7 @@ export const TestClearSelections: MtSelectStory = {
 
     await userEvent.click(canvas.getByTestId("select-clear-button"));
 
-    expect(args.change).toHaveBeenCalledWith([]);
+    expect(args.change).toHaveBeenCalledWith(null);
   },
 };
 
