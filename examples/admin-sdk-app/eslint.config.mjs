a7bba0e2356088d8c2edcf6d438768cfba30c669
Nils <haberkamp.n@gmail.com>
2025-05-09 14:16:56 +0200

    style(admin-sdk-app): format files

--- examples/admin-sdk-app/eslint.config.mjs
+++ examples/admin-sdk-app/eslint.config.mjs
@@ -24,5 +24,5 @@ export default typescriptEslint.config(
       },
     },
   },
-  eslintConfigPrettier,
+  eslintConfigPrettier
 );
--- examples/admin-sdk-app/src/frontend/cms/ex-dailymotion/ex-dailymotion-element.vue
+++ examples/admin-sdk-app/src/frontend/cms/ex-dailymotion/ex-dailymotion-element.vue
@@ -1,8 +1,6 @@
 <template>
   <div class="ex-dailymotion-element">
-    <h2>
-      Your dailymotion video stuff!
-    </h2>
+    <h2>Your dailymotion video stuff!</h2>
 
     <div class="sw-cms-el-dailymotion">
       <div class="sw-cms-el-dailymotion-iframe-wrapper">
@@ -19,64 +17,65 @@
 </template>
 
 <script setup lang="ts">
-import { ref, computed, onBeforeMount } from 'vue';
-import { data } from '@shopware-ag/meteor-admin-sdk';
-import EX_DAILYMOTION_CONSTANTS from './ex-dailymotion-constants';
+import { ref, computed, onBeforeMount } from "vue";
+import { data } from "@shopware-ag/meteor-admin-sdk";
+import EX_DAILYMOTION_CONSTANTS from "./ex-dailymotion-constants";
 
-const dataSelectors = [
-  'config.dailyUrl.value',
-  'config.dailyUrl.source',
-];
+const dataSelectors = ["config.dailyUrl.value", "config.dailyUrl.source"];
 
-const dailyUrlValue = ref<string|undefined>(undefined);
-const dailyUrlSource = ref<string>('static');
+const dailyUrlValue = ref<string | undefined>(undefined);
+const dailyUrlSource = ref<string>("static");
 
 const dailyUrl = computed(() => {
-  const dailymotionURL = dailyUrlValue.value || 'x8hc5d6';
+  const dailymotionURL = dailyUrlValue.value || "x8hc5d6";
 
   return `https://www.dailymotion.com/embed/video/${dailymotionURL}`;
 });
 
 const dataId = computed(() => {
   const params = new URLSearchParams(window.location.search);
-  const elementId = params.get('elementId');
+  const elementId = params.get("elementId");
 
   if (!elementId) {
     return EX_DAILYMOTION_CONSTANTS.PUBLISHING_KEY;
   }
 
-  return EX_DAILYMOTION_CONSTANTS.PUBLISHING_KEY + '__' + elementId;
+  return EX_DAILYMOTION_CONSTANTS.PUBLISHING_KEY + "__" + elementId;
 });
 
 onBeforeMount(async () => {
-  const value = await data.get({
+  const value = (await data.get({
     id: dataId.value,
     selectors: dataSelectors,
-  }) as {
-    'config.dailyUrl.value': string,
-    'config.dailyUrl.source': string,
+  })) as {
+    "config.dailyUrl.value": string;
+    "config.dailyUrl.source": string;
   };
 
   if (value) {
-    dailyUrlValue.value = value['config.dailyUrl.value'];
-    dailyUrlSource.value = value['config.dailyUrl.source'];
+    dailyUrlValue.value = value["config.dailyUrl.value"];
+    dailyUrlSource.value = value["config.dailyUrl.source"];
   }
 
   data.subscribe(
     dataId.value,
     // @ts-expect-error -- TS doesn't know about the subscriber
     elementSubscriber,
-  {
-    selectors: dataSelectors,
-  });
+    {
+      selectors: dataSelectors,
+    }
+  );
 });
 
-function elementSubscriber(response: { data: {
-  'config.dailyUrl.value': string,
-  'config.dailyUrl.source': string,
-}, id: string }): void {
-  dailyUrlSource.value = response.data['config.dailyUrl.source'];
-  dailyUrlValue.value = response.data['config.dailyUrl.value'];
+function elementSubscriber(response: {
+  data: {
+    "config.dailyUrl.value": string;
+    "config.dailyUrl.source": string;
+  };
+  id: string;
+}): void {
+  dailyUrlSource.value = response.data["config.dailyUrl.source"];
+  dailyUrlValue.value = response.data["config.dailyUrl.value"];
 }
 </script>
 
