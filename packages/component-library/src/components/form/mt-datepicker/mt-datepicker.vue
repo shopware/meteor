4bf766d7525c8c8057796b66165bbda22631ee31
alastair-simon <alastairfraser.work@gmail.com>
2025-05-14 16:40:02 +0200

    refactor: implement clearable button

--- packages/component-library/src/components/form/mt-datepicker/mt-datepicker.vue
+++ packages/component-library/src/components/form/mt-datepicker/mt-datepicker.vue
@@ -20,7 +20,7 @@
       :open="isDatepickerOpen"
       :teleport="true"
       :show-cancel="true"
-      :clearable="false"
+      :clearable="true"
       :auto-apply="true"
       :range="range"
       :format="formatDate"
@@ -58,7 +58,11 @@
 
     <template v-if="isTimeHintVisible">
       <!-- @deprecated tag:v5 remove field-hint class -->
-      <div class="mt-datepicker__hint field-hint" data-test="time-zone-hint" :style="{ gridArea: 'hint' }">
+      <div
+        class="mt-datepicker__hint field-hint"
+        data-test="time-zone-hint"
+        :style="{ gridArea: 'hint' }"
+      >
         <mt-icon name="solid-clock" class="mt-datepicker__hint-icon" />
         <p>{{ timeZone || "UTC" }}</p>
       </div>
@@ -189,6 +193,16 @@ export default defineComponent({
       required: false,
       default: false,
     },
+
+    /**
+     * Enables the date range selection feature.
+     * If true, the user can select a start and end date.
+     */
+    clearable: {
+      type: Boolean as PropType<boolean>,
+      required: false,
+      default: true,
+    },
   },
 
   data(): {
@@ -203,7 +217,9 @@ export default defineComponent({
 
   computed: {
     computedValue: {
-      get(): string | string[] {
+      get(): string | string[] | null {
+        if (!this.modelValue) return null;
+
         if (this.dateType === "time") {
           // Convert ISO string to object with hours, minutes, seconds
           const date = new Date(this.modelValue as string);
@@ -220,15 +236,21 @@ export default defineComponent({
         return this.modelValue;
       },
       set(newValue: Date | [Date, Date] | null) {
-        if (!newValue) return;
+        // Emit null if there is no value
+        if (!newValue ) {
+          this.$emit("update:modelValue", null);
+          return;
+        }
 
         // Handle date conversion for 'time' type
         if (this.dateType === "time") {
-          const isoFormattedDate = this.convertTimeToIso(newValue as unknown as {
-            hours: number;
-            minutes: number;
-            seconds: number;
-          });
+          const isoFormattedDate = this.convertTimeToIso(
+            newValue as unknown as {
+              hours: number;
+              minutes: number;
+              seconds: number;
+            },
+          );
 
           this.$emit("update:modelValue", isoFormattedDate);
           return;
@@ -287,11 +309,7 @@ export default defineComponent({
       }
     },
 
-    convertTimeToIso(time: {
-      hours: number;
-      minutes: number;
-      seconds: number;
-    }): string {
+    convertTimeToIso(time: { hours: number; minutes: number; seconds: number }): string {
       const date = new Date();
       date.setHours(time.hours, time.minutes, time.seconds);
       return date.toISOString();
@@ -541,7 +559,7 @@ export default defineComponent({
   display: none;
 }
 
-.dp__icon {
+.dp__button.dp__overlay_action svg {
   display: none;
 }
 
@@ -555,9 +573,7 @@ export default defineComponent({
 }
 
 .dp--clear-btn {
-  display: absolute;
-  z-index: 9999;
-  background: red;
+  right: var(--scale-size-48);
 }
 
 .mt-datepicker__hint {
