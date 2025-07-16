bde7c787e2ff3126fde39185fe704ffacfa6601c
alastair-simon <alastairfraser.work@gmail.com>
2025-07-16 22:09:29 +0200

    feat: implement base snackbar
    refactor: remove comments
    
    refactor: story namings
    
    docs: replace button with mt-button in story
    
    refactor: story template
    
    refactor: change story heading
    
    refactor: remove computed class mt-snackbar
    
    style: format code
    
    refactor: remove unused showTimer
    
    refactor: remove snackbar dissmissable prop
    
    refactor: replace id with createID
    
    docs: add comments
    
    refactor css with nesting
    
    refactor: css inside notification to nested
    
    refactor: adjust animation timing
    
    refactor: re-implement link component

---
+++ packages/component-library/src/components/feedback-indicator/mt-snackbar/mt-snackbar.vue
@@ -0,0 +1,61 @@
+<template>
+  <Teleport to="body">
+    <div class="mt-snackbar">
+      <TransitionGroup name="snackbars">
+        <mt-snackbar-notification
+          v-for="(snackbar, index) in snackbars"
+          :key="snackbar.id"
+          :snackbar="snackbar"
+          :style="{ '--num': index }"
+          :index="index"
+          @remove-snackbar="removeSnackbar"
+        />
+      </TransitionGroup>
+    </div>
+  </Teleport>
+</template>
+
+<script setup lang="ts">
+import MtSnackbarNotification from "./_internal/mt-snackbar-notification.vue";
+import { useSnackbar } from "./composables/use-snackbar";
+
+const { snackbars, removeSnackbar } = useSnackbar();
+</script>
+
+<style scoped>
+.mt-snackbar {
+  position: fixed;
+  bottom: var(--scale-size-16);
+  right: var(--scale-size-16);
+  z-index: 1600;
+  display: flex;
+  flex-direction: column;
+  gap: var(--scale-size-8);
+  pointer-events: none;
+
+  .snackbars-enter-from {
+    transform: translateX(100%);
+    opacity: 0;
+  }
+
+  .snackbars-leave-to {
+    transform: translateX(100%);
+    opacity: 0;
+  }
+
+  .snackbars-enter-active {
+    transition: all 0.3s;
+    transition-timing-function: cubic-bezier(0.7, 0.1, 0.45, 0.9);
+  }
+
+  .snackbars-leave-active {
+    transition: all 0.3s;
+    transition-timing-function: cubic-bezier(0.7, 0.1, 0.45, 0.9);
+  }
+
+  .snackbars-move {
+    transition: transform 0.3s;
+    transition-timing-function: cubic-bezier(0.7, 0.1, 0.45, 0.9);
+  }
+}
+</style>
