8021c46a1cee021894ba0cd44bd4fdb47b9c5d9b
Dennis Höllmann <d.hoellmann@shopware.com>
2025-07-17 16:18:17 +0200

    feat: implement custom apexcharts wrapper

--- packages/component-library/src/components/graphs/mt-graph/mt-graph.vue
+++ packages/component-library/src/components/graphs/mt-graph/mt-graph.vue
@@ -1,41 +1,101 @@
 <template>
-  <apexchart
-    :type="type"
-    :series="series"
-    :options="chartOptions"
-    :width="width"
-    :height="height"
-    :class="`mt-graph-${type}`"
-    data-testid="mt-graph"
-  />
+  <div
+    ref="chartContainer"
+    :class="`mt-graph mt-graph-${props.type}`"
+    :style="{ width: props.width, height: props.height }"
+  ></div>
 </template>
 
 <script setup lang="ts">
-import apexchart from "vue3-apexcharts";
 import type { ApexOptions } from "apexcharts";
 import { getDefaultOptions } from "./mt-graph-default-options";
 import { deepCopyObject, deepMergeObjects } from "@/utils/object";
-import { defineProps, computed } from "vue";
+import { computed, defineProps, onMounted, ref, watch, onBeforeUnmount } from "vue";
+import { templateRef } from "@vueuse/core";
 
 export type GraphOptions = ApexOptions;
 
-const props = defineProps<{
-  series: any[];
-  options?: GraphOptions;
-  type?: string;
-  width?: string | number;
-  height?: string | number;
-}>();
+const props = withDefaults(
+  defineProps<{
+    series: any[];
+    options?: GraphOptions;
+    type?: ApexChart["type"];
+    width?: string | number;
+    height?: string | number;
+  }>(),
+  {
+    options: () => ({}),
+    type: () => "area",
+    width: () => "100%",
+    height: () => "300px",
+  },
+);
+
+const chartContainer = templateRef("chartContainer");
+const chart = ref<ApexCharts>();
+
+const init = async () => {
+  const ApexCharts = (await import("apexcharts")).default;
+  if (!chartContainer.value) {
+    throw new Error("Chart container is not defined");
+  }
+  if (!chartOptions.value) {
+    throw new Error("Chart options are not defined");
+  }
+  if (chart.value) {
+    chart.value.destroy();
+  }
+  chart.value = new ApexCharts(chartContainer.value, { ...chartOptions.value, series: [] });
+  chart.value.render();
+  chart.value.updateSeries(props.series);
+};
+
+const destroy = () => {
+  if (chart.value) {
+    chart.value.destroy();
+    chart.value = undefined;
+  }
+};
+
+onMounted(init);
+onBeforeUnmount(destroy);
 
 const chartOptions = computed<ApexOptions>(() => {
-  const type = props.type ?? "area";
   const options = props.options;
-  const defaultOptions = getDefaultOptions(type);
-  const defaultOptionsCopy = deepCopyObject(defaultOptions);
+  const defaultOptions = getDefaultOptions(props.type);
 
-  if (!options || !(options instanceof Object)) {
-    return defaultOptionsCopy;
-  }
-  return deepMergeObjects<ApexOptions>(defaultOptionsCopy, options);
+  let mergedOptions = deepCopyObject(defaultOptions);
+  mergedOptions = deepMergeObjects<ApexOptions>(mergedOptions, options);
+  return {
+    ...mergedOptions,
+    chart: {
+      type: props.type,
+      width: props.width,
+      height: props.height,
+      ...mergedOptions.chart,
+    },
+  };
 });
+
+watch(
+  () => props.options,
+  () => {
+    if (chart.value) {
+      chart.value.updateOptions(chartOptions.value);
+    }
+  },
+  { deep: true },
+);
+
+watch(
+  () => props.series,
+  () => {
+    if (chart.value) {
+      chart.value.updateSeries(props.series);
+    }
+  },
+  { deep: true },
+);
+
+watch([() => props.type, () => props.width, () => props.height], init);
 </script>
