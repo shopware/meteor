755961d2d6ed44564eee0029fd814fb00ed522e1
Dennis Höllmann <d.hoellmann@shopware.com>
2025-07-17 16:23:50 +0200

    fix: factory setup

--- packages/component-library/src/components/graphs/mt-graph/mt-graph-default-options.ts
+++ packages/component-library/src/components/graphs/mt-graph/mt-graph-default-options.ts
@@ -1,57 +1,53 @@
 import type { ApexOptions } from "apexcharts";
 
-const optionsMap: Record<string, () => ApexOptions> = {
-  area: getDefaultAreaOptions,
-};
-
 export function getDefaultOptions(type: string): ApexOptions {
-  const getOptions = optionsMap[type];
-  if (getOptions) {
-    return getOptions();
-  }
-  return {};
+  const options = createOptions();
+
+  return options[type] ?? {};
 }
 
-function getDefaultAreaOptions(): ApexOptions {
+function createOptions(): Record<string, ApexOptions> {
   return {
-    noData: {
-      align: "center",
-      verticalAlign: "middle",
-    },
-    stroke: {
-      curve: "straight",
-      width: 2,
-    },
-    chart: {
-      toolbar: { show: false },
-      zoom: { enabled: false },
-    },
-    xaxis: {
-      tooltip: { enabled: false },
-      crosshairs: {
-        show: true,
-        width: 1,
-        stroke: {
-          color: "#2d2e32",
+    area: {
+      noData: {
+        align: "center",
+        verticalAlign: "middle",
+      },
+      stroke: {
+        curve: "straight",
+        width: 2,
+      },
+      chart: {
+        toolbar: { show: false },
+        zoom: { enabled: false },
+      },
+      xaxis: {
+        tooltip: { enabled: false },
+        crosshairs: {
+          show: true,
           width: 1,
-          dashArray: 0,
+          stroke: {
+            color: "#2d2e32",
+            width: 1,
+            dashArray: 0,
+          },
         },
       },
-    },
-    grid: {
-      show: true,
-      borderColor: "#cdced4",
-    },
-    colors: ["#0870ff"],
-    fill: {
-      type: "gradient",
-      gradient: {
-        type: "vertical",
-        opacityFrom: 0.7,
-        opacityTo: 0,
-        stops: [0, 100],
+      grid: {
+        show: true,
+        borderColor: "#cdced4",
       },
+      colors: ["#0870ff"],
+      fill: {
+        type: "gradient",
+        gradient: {
+          type: "vertical",
+          opacityFrom: 0.7,
+          opacityTo: 0,
+          stops: [0, 100],
+        },
+      },
+      dataLabels: { enabled: false },
     },
-    dataLabels: { enabled: false },
   };
 }
