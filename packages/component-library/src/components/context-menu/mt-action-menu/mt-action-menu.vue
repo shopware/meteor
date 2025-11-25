bc7f282640cdcf35978f09c27ca1a2fe39f01240
Nils Haberkamp <haberkamp.n@gmail.com>
2025-11-25 15:53:50 +0100

    feat(component-library): prototype action menu

---
+++ packages/component-library/src/components/context-menu/mt-action-menu/mt-action-menu.vue
@@ -0,0 +1,22 @@
+<template>
+    <dropdown-menu-content class="mt-action-menu" :side-offset="8" align="start">
+        <slot name="default" />
+    </dropdown-menu-content>
+</template> 
+
+<script setup lang="ts">
+import { DropdownMenuContent } from 'reka-ui';
+</script>
+
+<style>
+/* Find out why we can't scope styles */
+.mt-action-menu {
+    background-color: var(--color-elevation-surface-default);
+    border: 1px solid var(--color-border-secondary-default);
+    box-shadow: 0px 6px 24px -8px var(--color-elevation-shadow-default);
+    border-radius: var(--border-radius-m);
+    padding: var(--scale-size-4);
+
+    min-width: 320px
+}
+</style>
