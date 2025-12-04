ee6c8e0412517cfb5eb34efcda3b8ff3a62b8678
Bojan Rajh <117360292+bojanrajh@users.noreply.github.com>
2025-12-04 13:34:27 +0100

    Apply suggestion from @bojanrajh

--- .github/scripts/docs-after-clone.sh
+++ .github/scripts/docs-after-clone.sh
@@ -3,15 +3,6 @@ set -e
 
 [[ -z "$1" ]] && echo "Missing working directory argument" && exit 1
 
-#echo "Installing root npm"
-#pnpm -C $1 i
-
-#echo "Running root build"
-#pnpm -C $1 --filter "*/meteor-icon-kit" build
-
-#echo "Building figma"
-#pnpm -C $1 --filter "*/meteor-icon-kit" start
-
 echo "Copying icons"
 cp -R "$1/packages/icon-kit/icons" "$1/packages/icon-kit/docs/public"
 
