633dced2170d8f47c77343510e790109f644662e
Roland Kaibull <r.kaibull@shopware.com>
2026-04-09 06:46:51 +0200

    test(admin-sdk): add unit tests for telemetry.dispatch()

---
+++ packages/admin-sdk/src/telemetry/index.spec.ts
@@ -0,0 +1,65 @@
+import { dispatch, getSourceExtensionName } from './index';
+
+describe('telemetry', () => {
+  describe('dispatch', () => {
+    it('is a function', () => {
+      expect(typeof dispatch).toBe('function');
+    });
+  });
+
+  describe('getSourceExtensionName', () => {
+    beforeEach(() => {
+      window._swsdk = {
+        ...window._swsdk,
+        adminExtensions: {
+          'my-plugin': {
+            baseUrl: 'http://my-plugin.localhost',
+            permissions: {},
+          },
+          'another-extension': {
+            baseUrl: 'http://another.localhost',
+            permissions: {},
+          },
+        },
+      };
+    });
+
+    afterEach(() => {
+      window._swsdk = {
+        ...window._swsdk,
+        adminExtensions: {},
+      };
+    });
+
+    it('returns the technical name for a registered extension origin', () => {
+      const name = getSourceExtensionName('http://my-plugin.localhost/some/path');
+      expect(name).toBe('my-plugin');
+    });
+
+    it('returns the correct name when multiple extensions are registered', () => {
+      const name = getSourceExtensionName('http://another.localhost');
+      expect(name).toBe('another-extension');
+    });
+
+    it('returns undefined for an unknown origin', () => {
+      const name = getSourceExtensionName('http://unknown.localhost');
+      expect(name).toBeUndefined();
+    });
+
+    it('returns undefined for empty string', () => {
+      const name = getSourceExtensionName('');
+      expect(name).toBeUndefined();
+    });
+
+    it('returns undefined for undefined', () => {
+      const name = getSourceExtensionName(undefined);
+      expect(name).toBeUndefined();
+    });
+
+    it('returns undefined when origin matches the admin window (same origin)', () => {
+      // window.location.origin in jsdom is 'http://localhost'
+      const name = getSourceExtensionName(window.location.origin);
+      expect(name).toBeUndefined();
+    });
+  });
+});
