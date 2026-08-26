bf030928637e3d8bc3827499677505acd29b4d14
arnoldstoba <a.stoba@shopware.com>
2026-08-26 11:58:28 +0200

    fix(component-library): prevent number and unit field width collapse

--- packages/component-library/src/components/mt-slider/mt-slider.vue
+++ packages/component-library/src/components/mt-slider/mt-slider.vue
@@ -31,6 +31,7 @@
         :step="step"
         :disabled="disabled"
         :number-type="step % 1 === 0 ? 'int' : 'float'"
+        :show-controls="false"
         data-testid="left-number-field"
       />
       <div class="mt-slider__slider">
@@ -94,6 +95,7 @@
         :step="step"
         :disabled="disabled"
         :number-type="step % 1 === 0 ? 'int' : 'float'"
+        :show-controls="false"
         data-testid="right-number-field"
       />
     </template>
@@ -432,10 +434,6 @@ export default defineComponent({
 
 .mt-slider .mt-number-field__controls {
   flex-shrink: 1;
-}
-
-.mt-slider .mt-field--default .mt-field__controls {
-  display: none;
 }
 
 .mt-slider .mt-field--default input {
